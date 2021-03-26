bd08186e12dfbb3b786f31cb81fb372b2f99987a
openvela-robot <openvela-robot@xiaomi.com>
2021-03-26 17:45:31 +0800

    arch/arm: Fix the style warning
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/arm/src/nuc1xx/nuc_dumpgpio.c
+++ arch/arm/src/nuc1xx/nuc_dumpgpio.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * arch/arm/src/nuc/nuc_gpio.c
+ * arch/arm/src/nuc1xx/nuc_dumpgpio.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
--- arch/arm/src/nuc1xx/nuc_idle.c
+++ arch/arm/src/nuc1xx/nuc_idle.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- *  arch/arm/src/nuc1xx/nuc_idle.c
+ * arch/arm/src/nuc1xx/nuc_idle.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
