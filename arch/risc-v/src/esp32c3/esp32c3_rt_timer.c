7eeaa6837a8f88805cdb67deb9e25f67a0c343d4
openvela-robot <openvela-robot@xiaomi.com>
2021-05-07 05:09:33 +0800

    drivers/serial: Change the default value of SERIAL_NPOLLWAITERS to 4
    since the console is normally opened three time as stdin, stdout and
    stderr and remove the dependence on STANDARD_SERIAL because other type
    of serial drivers also support the poll through the same codebase.
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/risc-v/src/esp32c3/esp32c3_rt_timer.c
+++ arch/risc-v/src/esp32c3/esp32c3_rt_timer.c
@@ -253,7 +253,6 @@ static void stop_rt_timer(FAR struct rt_timer_s *timer)
 
 static void delete_rt_timer(FAR struct rt_timer_s *timer)
 {
-  int ret;
   irqstate_t flags;
 
   flags = enter_critical_section();
@@ -273,14 +272,6 @@ static void delete_rt_timer(FAR struct rt_timer_s *timer)
 
   list_add_after(&s_toutlist, &timer->list);
   timer->state = RT_TIMER_DELETE;
-
-  /* Wake up thread to process deleted timers */
-
-  ret = nxsem_post(&s_toutsem);
-  if (ret < 0)
-    {
-      tmrerr("ERROR: Failed to post sem ret=%d\n", ret);
-    }
 
 exit:
   leave_critical_section(flags);
@@ -396,17 +387,19 @@ static int rt_timer_thread(int argc, FAR char *argv[])
 
 static int rt_timer_isr(int irq, void *context, void *arg)
 {
-  int ret;
   irqstate_t flags;
   struct rt_timer_s *timer;
   uint64_t alarm;
   uint64_t counter;
-  bool wake = false;
   struct esp32c3_tim_dev_s *tim = s_esp32c3_tim_dev;
 
   /* Clear interrupt register status */
 
   ESP32C3_TIM_ACKINT(tim);
+
+  /* Wake up thread to process timeout timers */
+
+  nxsem_post(&s_toutsem);
 
   flags = enter_critical_section();
 
@@ -435,7 +428,6 @@ static int rt_timer_isr(int irq, void *context, void *arg)
           list_delete(&timer->list);
           timer->state = RT_TIMER_TIMEOUT;
           list_add_after(&s_toutlist, &timer->list);
-          wake = true;
 
           /* Check if thers is timer running */
 
@@ -447,22 +439,8 @@ static int rt_timer_isr(int irq, void *context, void *arg)
               alarm = timer->alarm;
 
               ESP32C3_TIM_SETALRVL(tim, alarm);
+              ESP32C3_TIM_SETALRM(tim, true);
             }
-        }
-
-      /* If there is timer in list, alarm should be enable */
-
-      ESP32C3_TIM_SETALRM(tim, true);
-    }
-
-  if (wake)
-    {
-      /* Wake up thread to process timeout timers */
-
-      ret = nxsem_post(&s_toutsem);
-      if (ret < 0)
-        {
-          tmrerr("ERROR: Failed to post sem ret=%d\n", ret);
         }
     }
 
--- binfmt/libelf/libelf_init.c
+++ binfmt/libelf/libelf_init.c
@@ -161,6 +161,7 @@ int elf_init(FAR const char *filename, FAR struct elf_loadinfo_s *loadinfo)
   if (ret < 0)
     {
       berr("Failed to read ELF header: %d\n", ret);
+      nx_close(loadinfo->filfd);
       return ret;
     }
 
@@ -180,6 +181,7 @@ int elf_init(FAR const char *filename, FAR struct elf_loadinfo_s *loadinfo)
        */
 
       berr("Bad ELF header: %d\n", ret);
+      nx_close(loadinfo->filfd);
       return ret;
     }
 
