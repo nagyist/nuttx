08e989d9727403aca6ecb6f3dc8d410762fdef3e
openvela-robot <openvela-robot@xiaomi.com>
2021-06-10 15:59:14 +0800

    sim: Fix posix_memalign mapping error.
    N/A
    
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- arch/xtensa/include/irq.h
+++ arch/xtensa/include/irq.h
@@ -192,11 +192,7 @@ static inline void xtensa_setps(uint32_t ps)
 {
   __asm__ __volatile__
   (
-    "wsr %0, PS \n"
-    "rsync \n"
-    :
-    : "r"(ps)
-    : "memory"
+    "wsr %0, PS"  : : "r"(ps)
   );
 }
 
@@ -206,11 +202,7 @@ static inline void up_irq_restore(uint32_t ps)
 {
   __asm__ __volatile__
   (
-    "wsr %0, PS \n"
-    "rsync \n"
-    :
-    : "r"(ps)
-    : "memory"
+    "wsr %0, PS"  : : "r"(ps)
   );
 }
 
