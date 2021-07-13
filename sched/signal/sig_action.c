547989d995e4b9098502abcbbfb436529d6a4bba
openvela-robot <openvela-robot@xiaomi.com>
2021-07-13 21:56:29 +0800

    sched/signal: add spinlock to g_sigfreeaction
    To avoid nxsig_alloc_action() & nxsig_release_action() competition
    
    Signed-off-by: ligd <liguiding1@xiaomi.com>

--- sched/signal/sig_action.c
+++ sched/signal/sig_action.c
@@ -33,17 +33,10 @@
 #include <errno.h>
 
 #include <nuttx/irq.h>
-#include <nuttx/spinlock.h>
 
 #include "sched/sched.h"
 #include "group/group.h"
 #include "signal/signal.h"
-
-/****************************************************************************
- * Private Data
- ****************************************************************************/
-
-static spinlock_t g_sigaction_spin;
 
 /****************************************************************************
  * Private Functions
@@ -69,14 +62,14 @@ static void nxsig_alloc_actionblock(void)
   sigact = kmm_malloc((sizeof(sigactq_t)) * NUM_SIGNAL_ACTIONS);
   if (sigact != NULL)
     {
-      flags = spin_lock_irqsave(&g_sigaction_spin);
+      flags = spin_lock_irqsave(NULL);
 
       for (i = 0; i < NUM_SIGNAL_ACTIONS; i++)
         {
           sq_addlast((FAR sq_entry_t *)sigact++, &g_sigfreeaction);
         }
 
-      spin_unlock_irqrestore(&g_sigaction_spin, flags);
+      spin_unlock_irqrestore(NULL, flags);
     }
 }
 
@@ -95,9 +88,9 @@ static FAR sigactq_t *nxsig_alloc_action(void)
 
   /* Try to get the signal action structure from the free list */
 
-  flags = spin_lock_irqsave(&g_sigaction_spin);
+  flags = spin_lock_irqsave(NULL);
   sigact = (FAR sigactq_t *)sq_remfirst(&g_sigfreeaction);
-  spin_unlock_irqrestore(&g_sigaction_spin, flags);
+  spin_unlock_irqrestore(NULL, flags);
 
   /* Check if we got one. */
 
@@ -109,9 +102,9 @@ static FAR sigactq_t *nxsig_alloc_action(void)
 
       /* And try again */
 
-      flags = spin_lock_irqsave(&g_sigaction_spin);
+      flags = spin_lock_irqsave(NULL);
       sigact = (FAR sigactq_t *)sq_remfirst(&g_sigfreeaction);
-      spin_unlock_irqrestore(&g_sigaction_spin, flags);
+      spin_unlock_irqrestore(NULL, flags);
       DEBUGASSERT(sigact);
     }
 
@@ -410,7 +403,7 @@ void nxsig_release_action(FAR sigactq_t *sigact)
 
   /* Just put it back on the free list */
 
-  flags = spin_lock_irqsave(&g_sigaction_spin);
+  flags = spin_lock_irqsave(NULL);
   sq_addlast((FAR sq_entry_t *)sigact, &g_sigfreeaction);
-  spin_unlock_irqrestore(&g_sigaction_spin, flags);
+  spin_unlock_irqrestore(NULL, flags);
 }
