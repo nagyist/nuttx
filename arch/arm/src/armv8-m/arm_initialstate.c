227ada9b76d75478184dab3e2611e59797de6e9f
openvela-robot <openvela-robot@xiaomi.com>
2022-04-11 19:44:26 +0800

    sched/init: drivers_initialize() should be late than up_initialize()
    up_initialize
    |
     ->up_serialinit
       |
        ->uart_register  /* ("/dev/console", &CONSOLE_DEV); */
    
    drivers_initialize
    |
     ->syslog_console_init
       |
        ->register_driver /* ("/dev/console", &g_consoleops, 0666, NULL); */
    
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- arch/arm/src/armv8-m/arm_initialstate.c
+++ arch/arm/src/armv8-m/arm_initialstate.c
@@ -32,6 +32,8 @@
 #include <arch/armv8-m/nvicpri.h>
 
 #include "arm_internal.h"
+#include "arm_arch.h"
+
 #include "psr.h"
 #include "exc_return.h"
 
@@ -155,7 +157,7 @@ void up_initial_state(struct tcb_s *tcb)
 
 #if !defined(CONFIG_ARMV8M_LAZYFPU) && defined(CONFIG_ARCH_FPU)
 
-  xcp->regs[REG_FPSCR] |= ARMV8M_FPSCR_LTPSIZE_NONE;
+  xcp->regs[REG_FPSCR] = 0;      /* REVISIT: Initial FPSCR should be configurable */
   xcp->regs[REG_FP_RESERVED] = 0;
 
 #endif /* !CONFIG_ARMV8M_LAZYFPU && CONFIG_ARCH_FPU */
