f9c9e118044d28207529964da95e9ca94994cab4
openvela-robot <openvela-robot@xiaomi.com>
2022-06-19 20:48:31 +0800

    wireless/bcm43xxx: prefer HPWORK if bcmf task has higher priority than HPWORK
    Use high priority queue if the bcmf daemon task has a higher priority
    than HPWORK, which will bring better performance especially on devices
    that focus on real-time of network.
    
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- drivers/power/pm.h
+++ drivers/power/pm.h
@@ -57,8 +57,7 @@ struct pm_domain_s
 
   /* The power state lock count */
 
-  struct dq_queue_s wakelock[PM_COUNT];
-  struct wdog_s wdog[PM_COUNT];
+  uint16_t stay[PM_COUNT];
 
   /* Auto update or not */
 
@@ -73,14 +72,6 @@ struct pm_domain_s
   /* A pointer to the PM governor instance */
 
   FAR const struct pm_governor_s *governor;
-
-  /* This semaphore manages mutually exclusive access to the domain state.
-   * It must be initialized to the value 1.
-   */
-
-  sem_t sem;
-  pid_t holder;
-  unsigned int count;
 };
 
 /* This structure encapsulates all of the global data used by the PM system */
@@ -132,12 +123,9 @@ EXTERN struct pm_global_s g_pmglobals;
  * Description:
  *   Lock the power management operation.
  *
- * Input Parameters:
- *   domain - The PM domain to lock
- *
  ****************************************************************************/
 
-irqstate_t pm_lock(int domain);
+irqstate_t pm_lock(void);
 
 /****************************************************************************
  * Name: pm_unlock
@@ -145,28 +133,9 @@ irqstate_t pm_lock(int domain);
  * Description:
  *   Unlock the power management operation.
  *
- * Input Parameters:
- *   domain - The PM domain to unlock
- *
  ****************************************************************************/
 
-void pm_unlock(int domain, irqstate_t flags);
-
-/****************************************************************************
- * Name: pm_wakelock_global_init
- *
- * Description:
- *   This function is called to setup global wakelock when system init
- *
- * Input Parameters:
- *   None
- *
- * Returned Value:
- *   None
- *
- ****************************************************************************/
-
-void pm_wakelock_global_init(void);
+void pm_unlock(irqstate_t flags);
 
 #undef EXTERN
 #if defined(__cplusplus)
