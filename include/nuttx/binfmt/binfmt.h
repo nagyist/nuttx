250c273fd00fd9cf0598b75061be93cffa0edb41
openvela-robot <openvela-robot@xiaomi.com>
2022-07-08 15:43:41 +0800

    wifi: parse channel from IE when ctl_ch is 0
    Signed-off-by: zhanghongyu <zhanghongyu@xiaomi.com>

--- include/nuttx/binfmt/binfmt.h
+++ include/nuttx/binfmt/binfmt.h
@@ -80,11 +80,12 @@ struct binary_s
 #ifdef CONFIG_ARCH_ADDRENV
   /* Address environment.
    *
-   * addrenv - This is the handle created by up_addrenv_create() that can be
+   * addrenv - This is the handle created by addrenv_allocate() that can be
    *   used to manage the tasks address space.
    */
 
-  addrenv_t addrenv;                   /* Address environment */
+  FAR addrenv_t *addrenv;              /* Address environment */
+  FAR addrenv_t *oldenv;               /* Saved address environment */
 #endif
 
   size_t mapsize;                      /* Size of the mapped address region (needed for munmap) */
@@ -269,8 +270,7 @@ int unload_module(FAR struct binary_s *bin);
 int exec_module(FAR struct binary_s *binp,
                 FAR const char *filename, FAR char * const *argv,
                 FAR char * const *envp,
-                FAR const posix_spawn_file_actions_t *actions,
-                bool spawn);
+                FAR const posix_spawn_file_actions_t *actions);
 
 /****************************************************************************
  * Name: exec
