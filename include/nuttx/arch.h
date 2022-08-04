1fd35912fd719489efae924bc05e6fcda2998368
openvela-robot <openvela-robot@xiaomi.com>
2022-08-04 14:54:31 +0800

    optimize UNUSED macro
    Signed-off-by: liyi <liyi25@xiaomi.com>

--- include/nuttx/arch.h
+++ include/nuttx/arch.h
@@ -1852,7 +1852,6 @@ int up_tls_size(void);
  ****************************************************************************/
 
 #ifdef CONFIG_SCHED_THREAD_LOCAL
-struct tls_info_s;
 void up_tls_initialize(FAR struct tls_info_s *info);
 #else
 #define up_tls_initialize(x)
