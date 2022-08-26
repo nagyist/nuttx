666df5cf2a27fc0da797245fb303357295a2dcce
openvela-robot <openvela-robot@xiaomi.com>
2022-08-26 16:52:32 +0800

    sched: Fix macro "SCHED_NOTE_PRINTF" requires 3 argumnets, but only 1 given
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- net/tcp/tcp_send_buffered.c
+++ net/tcp/tcp_send_buffered.c
@@ -1232,11 +1232,6 @@ ssize_t psock_tcp_send(FAR struct socket *psock, FAR const void *buf,
             tcp_send_gettimeout(start, timeout));
           if (ret < 0)
             {
-              if (ret == -ETIMEDOUT)
-                {
-                  ret = -EAGAIN;
-                }
-
               goto errout_with_lock;
             }
         }
@@ -1290,9 +1285,13 @@ ssize_t psock_tcp_send(FAR struct socket *psock, FAR const void *buf,
 
               nerr("ERROR: Failed to allocate write buffer\n");
 
-              if (nonblock || timeout != UINT_MAX)
+              if (nonblock)
                 {
                   ret = -EAGAIN;
+                }
+              else if (timeout != UINT_MAX)
+                {
+                  ret = -ETIMEDOUT;
                 }
               else
                 {
