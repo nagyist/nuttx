4e3a3dba3a6c73cc7a55e32e38d198ac4df004d1
openvela-robot <openvela-robot@xiaomi.com>
2022-09-18 17:25:05 +0800

    arm/rtl8720c: Remove up_trigger_irq since it is implemented in arm_trigger_irq.c
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/arm/src/armv7-a/arm_cpustart.c
+++ arch/arm/src/armv7-a/arm_cpustart.c
@@ -160,8 +160,7 @@ int up_cpu_start(int cpu)
 
   /* Execute SGI1 */
 
-  up_trigger_irq(GIC_IRQ_SGI1, (1 << cpu));
-  return OK;
+  return arm_cpu_sgi(GIC_IRQ_SGI1, (1 << cpu));
 }
 
 #endif /* CONFIG_SMP */
