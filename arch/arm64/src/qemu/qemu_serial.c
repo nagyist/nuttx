eb5d71020c9e88f3d4429d36f6432238cde9be81
Xiang Xiao <xiaoxiang@xiaomi.com>
2023-03-04 13:56:49 +0800

    arch/boards: Rename up_lowputc to [arm64|renesas]_lowputc
    follow other arch coding style
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/arm64/src/qemu/qemu_serial.c
+++ arch/arm64/src/qemu/qemu_serial.c
@@ -821,10 +821,10 @@ int up_putc(int ch)
     {
       /* Add CR */
 
-      up_lowputc('\r');
+      arm64_lowputc('\r');
     }
 
-  up_lowputc((uint8_t)ch);
+  arm64_lowputc((uint8_t)ch);
   return ch;
 }
 
@@ -868,10 +868,10 @@ int up_putc(int ch)
     {
       /* Add CR */
 
-      up_lowputc('\r');
+      arm64_lowputc('\r');
     }
 
-  up_lowputc((uint8_t)ch);
+  arm64_lowputc((uint8_t)ch);
   return ch;
 }
 
