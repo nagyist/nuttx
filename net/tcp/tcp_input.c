da8c8ab58c262caed5e11cde36e1bc9316039432
openvela-robot <openvela-robot@xiaomi.com>
2023-03-24 16:41:38 +0800

    net/inet: Only setup poll for UDP when s_type == SOCK_DGRAM
    Problem:
    A SOCK_CTRL socket may be led to udp_pollsetup but never reaches
    udp_pollteardown, it seems that we shouldn't call udp_pollsetup for
    other socket types.
    
    Signed-off-by: Zhe Weng <wengzhe@xiaomi.com>

--- net/tcp/tcp_input.c
+++ net/tcp/tcp_input.c
@@ -586,6 +586,13 @@ static void tcp_parse_option(FAR struct net_driver_s *dev,
 
           tmp16 = ((uint16_t)IPDATA(tcpiplen + 2 + i) << 8) |
                    (uint16_t)IPDATA(tcpiplen + 3 + i);
+#ifdef CONFIG_NET_TCPPROTO_OPTIONS
+          if (conn->user_mss > 0 && conn->user_mss < tcp_mss)
+            {
+              tcp_mss = conn->user_mss;
+            }
+#endif
+
           conn->mss = tmp16 > tcp_mss ? tcp_mss : tmp16;
         }
 #ifdef CONFIG_NET_TCP_WINDOW_SCALE
@@ -748,9 +755,9 @@ static void tcp_input(FAR struct net_driver_s *dev, uint8_t domain,
 #endif
 
 #if defined(CONFIG_NET_IPv4) && defined(CONFIG_NET_IPv6)
-      if (tcp_islistener(&uaddr, tmp16, domain))
+      if ((conn = tcp_findlistener(&uaddr, tmp16, domain)) != NULL)
 #else
-      if (tcp_islistener(&uaddr, tmp16))
+      if ((conn = tcp_findlistener(&uaddr, tmp16)) != NULL)
 #endif
         {
           /* We matched the incoming packet with a connection in LISTEN.
@@ -762,7 +769,7 @@ static void tcp_input(FAR struct net_driver_s *dev, uint8_t domain,
            * any user application to accept it.
            */
 
-          conn = tcp_alloc_accept(dev, tcp);
+          conn = tcp_alloc_accept(dev, tcp, conn);
           if (conn)
             {
               /* The connection structure was successfully allocated and has
@@ -1545,15 +1552,7 @@ found:
 
         if (dev->d_len > 0)
           {
-            /* Due to RFC 2525, Section 2.17, we SHOULD send RST if we can no
-             * longer read any received data. Also set state into TCP_CLOSED
-             * because the peer will not send FIN after RST received.
-             *
-             * TODO: Modify shutdown behavior to allow read in FIN_WAIT.
-             */
-
-            conn->tcpstateflags = TCP_CLOSED;
-            tcp_reset(dev, conn);
+            tcp_send(dev, conn, TCP_ACK, tcpiplen);
             return;
           }
 
@@ -1580,13 +1579,7 @@ found:
 
         if (dev->d_len > 0)
           {
-            /* Due to RFC 2525, Section 2.17, we SHOULD send RST if we can no
-             * longer read any received data. Also set state into TCP_CLOSED
-             * because the peer will not send FIN after RST received.
-             */
-
-            conn->tcpstateflags = TCP_CLOSED;
-            tcp_reset(dev, conn);
+            tcp_send(dev, conn, TCP_ACK, tcpiplen);
             return;
           }
 
