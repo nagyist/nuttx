203f25a61ff6a35ef00395545fae2f235a7d83ae
openvela-robot <openvela-robot@xiaomi.com>
2023-06-02 14:14:01 +0800

    usrsock/dns: add sanity check before send dns event
    Signed-off-by: chao an <anchao@xiaomi.com>

--- mm/mm_heap/mm_malloc.c
+++ mm/mm_heap/mm_malloc.c
@@ -96,10 +96,9 @@ void mm_mempool_dump_handle(FAR struct mempool_s *pool, FAR void *arg)
   struct mempoolinfo_s info;
 
   mempool_info(pool, &info);
-  mwarn("%9lu%11lu%9lu%9lu%9lu%9lu%9zu\n",
+  mwarn("%9lu%11lu%9lu%9lu%9lu%9lu\n",
         info.sizeblks, info.arena, info.aordblks,
-        info.ordblks, info.iordblks,
-        info.nwaiter, pool->nexpend);
+        info.ordblks, info.iordblks, info.nwaiter);
 }
 #endif
 
@@ -283,7 +282,7 @@ FAR void *mm_malloc(FAR struct mm_heap_s *heap, size_t size)
 
       mwarn("WARNING: Allocation failed, size %zu\n", alignsize);
 #ifdef CONFIG_MM_DUMP_ON_FAILURE
-      minfo = mm_mallinfo(heap);
+      mm_mallinfo(heap, &minfo);
       mwarn("Total:%d, used:%d, free:%d, largest:%d, nused:%d, nfree:%d\n",
             minfo.arena, minfo.uordblks, minfo.fordblks,
             minfo.mxordblk, minfo.aordblks, minfo.ordblks);
@@ -292,8 +291,9 @@ FAR void *mm_malloc(FAR struct mm_heap_s *heap, size_t size)
       mm_dump_handler(NULL, heap);
 #  endif
 #  if CONFIG_MM_HEAP_MEMPOOL_THRESHOLD != 0
-      mwarn("%11s%9s%9s%9s%9s%9s%9s\n", "bsize", "total", "nused",
-            "nfree", "nifree", "nwaiter", "nexpend");
+      mwarn("%11s%9s%9s%9s%9s%9s\n",
+            "bsize", "total", "nused",
+            "nfree", "nifree", "nwaiter");
       mempool_multiple_foreach(heap->mm_mpool,
                                mm_mempool_dump_handle, NULL);
 #  endif
