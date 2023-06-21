9d1d26efb8bcbd72db87a0b0372486ccbcf6a9d3
openvela-robot <openvela-robot@xiaomi.com>
2023-06-21 16:48:46 +0800

    note: remove NOTERAM_GETTASKNAME
    Signed-off-by: yinshengkai <yinshengkai@xiaomi.com>

--- drivers/can/can.c
+++ drivers/can/can.c
@@ -1242,7 +1242,6 @@ int can_receive(FAR struct can_dev_s *dev, FAR struct can_hdr_s *hdr,
   int                      nexttail;
   int                      errcode = -ENOMEM;
   int                      i;
-  int                      j;
   int                      sval;
   int                      ret;
 
@@ -1292,7 +1291,7 @@ int can_receive(FAR struct can_dev_s *dev, FAR struct can_hdr_s *hdr,
               memcpy(&waitmsg->cm_hdr, hdr, sizeof(struct can_hdr_s));
 
               nbytes = can_dlc2bytes(hdr->ch_dlc);
-              for (j = 0, dest = waitmsg->cm_data; j < nbytes; j++)
+              for (i = 0, dest = waitmsg->cm_data; i < nbytes; i++)
                 {
                   *dest++ = *data++;
                 }
