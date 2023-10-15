4082950bcdc4444887f41e909f4ef527880158bf
openvela-robot <openvela-robot@xiaomi.com>
2023-10-15 14:27:39 +0800

    sched: Remove the unused tcb argument from group_setupidlefiles
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- mm/tlsf/mm_tlsf.c
+++ mm/tlsf/mm_tlsf.c
@@ -105,6 +105,7 @@ struct mm_heap_s
 
   /* Free delay list, for some situation can't do free immdiately */
 
+  spinlock_t mm_spinlock;
   struct mm_delaynode_s *mm_delaylist[CONFIG_SMP_NCPUS];
 
 #if defined(CONFIG_FS_PROCFS) && !defined(CONFIG_FS_PROCFS_EXCLUDE_MEMINFO)
@@ -177,12 +178,12 @@ static void add_delaylist(FAR struct mm_heap_s *heap, FAR void *mem)
 
   /* Delay the deallocation until a more appropriate time. */
 
-  flags = up_irq_save();
+  flags = spin_lock_irqsave(&heap->mm_spinlock);
 
   tmp->flink = heap->mm_delaylist[up_cpu_index()];
   heap->mm_delaylist[up_cpu_index()] = tmp;
 
-  up_irq_restore(flags);
+  spin_unlock_irqrestore(&heap->mm_spinlock, flags);
 #endif
 }
 
@@ -198,12 +199,12 @@ static void free_delaylist(FAR struct mm_heap_s *heap)
 
   /* Move the delay list to local */
 
-  flags = up_irq_save();
+  flags = spin_lock_irqsave(&heap->mm_spinlock);
 
   tmp = heap->mm_delaylist[up_cpu_index()];
   heap->mm_delaylist[up_cpu_index()] = NULL;
 
-  up_irq_restore(flags);
+  spin_unlock_irqrestore(&heap->mm_spinlock, flags);
 
   /* Test if the delayed is empty */
 
