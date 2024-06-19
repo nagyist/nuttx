f20fe001c08981d8c137bc8630a52676e5e4e129
Huang Qi <huangqi3@xiaomi.com>
2024-06-19 09:29:38 +0800

    riscv: Improve exception and irq mapping
    Allow chip to define the custom exception on demand.
    
    Signed-off-by: Huang Qi <huangqi3@xiaomi.com>

--- arch/risc-v/src/common/riscv_exception.c
+++ arch/risc-v/src/common/riscv_exception.c
@@ -55,7 +55,22 @@ static const char *g_reasons_str[RISCV_MAX_EXCEPTION + 1] =
   "Instruction page fault",
   "Load page fault",
   "Reserved",
-  "Store/AMO page fault"
+  "Store/AMO page fault",
+#if RISCV_MAX_EXCEPTION > 15
+  "Reserved",
+#endif
+#if RISCV_MAX_EXCEPTION > 16
+  "Reserved",
+#endif
+#if RISCV_MAX_EXCEPTION > 17
+  "Software check",
+#endif
+#if RISCV_MAX_EXCEPTION > 18
+  "Hardware error"
+#endif
+#ifdef RISCV_CUSTOM_EXCEPTION_REASONS
+  RISCV_CUSTOM_EXCEPTION_REASONS
+#endif
 };
 
 /****************************************************************************
@@ -131,7 +146,7 @@ void riscv_exception_attach(void)
 
   irq_attach(RISCV_IRQ_INSTRUCTIONPF, riscv_exception, NULL);
   irq_attach(RISCV_IRQ_LOADPF, riscv_exception, NULL);
-  irq_attach(RISCV_IRQ_RESERVED, riscv_exception, NULL);
+  irq_attach(RISCV_IRQ_RESERVED14, riscv_exception, NULL);
   irq_attach(RISCV_IRQ_STOREPF, riscv_exception, NULL);
 
 #ifdef CONFIG_SMP
