dafa769dc7cf573a9b682f58982a30485d7eec4c
Michal Lenc <michallenc@seznam.cz>
2024-07-09 10:45:43 +0800

    can: enhance bit timing ioctl to set both nominal and data bit timing
    This adds field type to canioc_bittiming_s structure that allows
    to set/obtain bit timing for both CAN CC and CAN FD.
    CANIOC_GET_BITTIMING is now bidirectional: user specifies type field
    and gets other fields from the controller.
    
    The commit also updates current CAN FD capable controllers using the
    ioctl. The type is not checked for classical CAN only controllers
    and nominal bit timing is returned regardless of type value.
    
    Signed-off-by: Michal Lenc <michallenc@seznam.cz>

--- include/nuttx/can/can.h
+++ include/nuttx/can/can.h
@@ -523,6 +523,11 @@
 
 #define CAN_TRANSVSTATE_NORMAL    2
 
+/* CAN bit timing support ***************************************************/
+
+#define CAN_BITTIMING_DATA        0  /* Specifies nominal bittiming */
+#define CAN_BITTIMING_NOMINAL     1  /* Specifies data bittiming */
+
 /****************************************************************************
  * Public Types
  ****************************************************************************/
@@ -825,6 +830,13 @@ struct canioc_bittiming_s
   uint8_t               bt_tseg1;        /* TSEG1 in time quanta */
   uint8_t               bt_tseg2;        /* TSEG2 in time quanta */
   uint8_t               bt_sjw;          /* Synchronization Jump Width in time quanta */
+#ifdef CONFIG_CAN_FD
+  uint8_t               bt_type;         /* Nominal/Data bit timing. This is
+                                          * used to specify which bit timing
+                                          * should be set/obtained. Applies
+                                          * only if CAN FD is configured.
+                                          */
+#endif
 };
 
 /* CANIOC_GET_CONNMODES/CANIOC_SET_CONNMODES:
