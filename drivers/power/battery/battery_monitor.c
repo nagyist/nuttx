4800fdf433e59f0dbdc92b06cfe38c99e78a0be6
dongjiuzhu1 <dongjiuzhu1@xiaomi.com>
2024-11-19 21:00:04 +0800

    drivers/battery: fix event lost before service open
    Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

--- drivers/power/battery/battery_monitor.c
+++ drivers/power/battery/battery_monitor.c
@@ -529,18 +529,18 @@ int battery_monitor_changed(FAR struct battery_monitor_dev_s *dev,
 
   /* Event happen too early? */
 
+  ret = nxmutex_lock(&dev->batlock);
+  if (ret < 0)
+    {
+      return ret;
+    }
+
   if (list_is_clear(&dev->flist))
     {
       /* Yes, record it and return directly */
 
       dev->mask |= mask;
-      return 0;
-    }
-
-  ret = nxmutex_lock(&dev->batlock);
-  if (ret < 0)
-    {
-      return ret;
+      goto out;
     }
 
   dev->mask |= mask;
@@ -551,6 +551,7 @@ int battery_monitor_changed(FAR struct battery_monitor_dev_s *dev,
       priv->mask |= mask;
     }
 
+out:
   nxmutex_unlock(&dev->batlock);
   return OK;
 }
