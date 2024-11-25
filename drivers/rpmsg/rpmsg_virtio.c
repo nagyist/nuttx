777738105f2112bb5c84885ae48333176baceeef
Bowen Wang <wangbowen6@xiaomi.com>
2024-11-25 22:45:40 +0800

    drivers/rpmsg/rpmsg.c: acquire rdev->lock when access the rdev->endpoints
    Because the ept may has been destroied when the ept->ns_unbind_cb are
    calling.
    
    Signed-off-by: Bowen Wang <wangbowen6@xiaomi.com>

--- drivers/rpmsg/rpmsg_virtio.c
+++ drivers/rpmsg/rpmsg_virtio.c
@@ -495,8 +495,7 @@ static void rpmsg_virtio_dump(FAR struct rpmsg_s *rpmsg)
       return;
     }
 
-  if (up_interrupt_context() || sched_idletask() ||
-      nxmutex_is_hold(&rdev->lock))
+  if (up_interrupt_context() || sched_idletask())
     {
       needlock = false;
     }
