cd699e41d0ce62c518cf4b5fabe22dc1d979d19e
guohao15 <guohao15@xiaomi.com>
2024-12-13 16:35:23 +0800

    Revert "Revert "fs/inode: add pre-allocated task files to avoid allocator access""
    This reverts commit d617b771dd0d5b5d8899f4966a4f60a993d0f62d.
    
    Reason for revert: <bugfix>
    
    Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

--- fs/inode/fs_files.c
+++ fs/inode/fs_files.c
@@ -198,7 +198,7 @@ static int files_extend(FAR struct filelist *list, size_t row)
 
   spin_unlock_irqrestore(NULL, flags);
 
-  if (tmp != NULL)
+  if (tmp != NULL && tmp != &list->fl_prefile)
     {
       fs_heap_free(tmp);
     }
@@ -363,7 +363,14 @@ static int nx_dup3_from_tcb(FAR struct tcb_s *tcb, int fd1, int fd2,
 
 void files_initlist(FAR struct filelist *list)
 {
+  /* The first row will reuse pre-allocated files, which will avoid
+   * unnecessary allocator accesses during file initialization.
+   */
+
+  list->fl_rows = 1;
   list->fl_crefs = 1;
+  list->fl_files = &list->fl_prefile;
+  list->fl_prefile = list->fl_prefiles;
 }
 
 /****************************************************************************
@@ -500,11 +507,16 @@ void files_putlist(FAR struct filelist *list)
           file_close(&list->fl_files[i][j]);
         }
 
-      fs_heap_free(list->fl_files[i]);
-      list->fl_rows--;
+      if (i != 0)
+        {
+          fs_heap_free(list->fl_files[i]);
+        }
     }
 
-  fs_heap_free(list->fl_files);
+  if (list->fl_files != &list->fl_prefile)
+    {
+      fs_heap_free(list->fl_files);
+    }
 }
 
 /****************************************************************************
