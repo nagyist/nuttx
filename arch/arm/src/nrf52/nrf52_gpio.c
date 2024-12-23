67bd7f6f00609a9285ce5565364751922a965a6a
hujun5 <hujun5@xiaomi.com>
2024-12-23 13:51:16 +0800

    use small lock in following files:
    arch/arm/src/kinetis/kinetis_serial.c
    arch/arm/src/kl/kl_serial.c
    arch/arm/src/lc823450/lc823450_irq.c
    arch/arm/src/lc823450/lc823450_syscontrol.c
    arch/arm/src/lpc54xx/lpc54_serial.c
    arch/arm/src/max326xx/max32660/max32660_dma.c
    arch/arm/src/max326xx/max32660/max32660_gpio.c
    arch/arm/src/max326xx/max32660/max32660_lowputc.c
    arch/arm/src/max326xx/max32660/max32660_serial.c
    arch/arm/src/mx8mp/mx8mp_serial.c
    arch/arm/src/nrf52/nrf52_gpio.c
    arch/arm/src/nrf53/nrf53_gpio.c
    arch/arm/src/nrf91/nrf91_gpio.c
    arch/arm/src/rp2040/rp2040_uart.c
    
    Signed-off-by: hujun5 <hujun5@xiaomi.com>

--- arch/arm/src/nrf52/nrf52_gpio.c
+++ arch/arm/src/nrf52/nrf52_gpio.c
@@ -37,6 +37,12 @@
 #include "nrf52_gpio.h"
 
 /****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+static spinlock_t g_nrf52_gpio_lock = SP_UNLOCKED;
+
+/****************************************************************************
  * Private Functions
  ****************************************************************************/
 
@@ -273,7 +279,7 @@ int nrf52_gpio_config(nrf52_pinset_t cfgset)
 
       pin = GPIO_PIN_DECODE(cfgset);
 
-      flags = spin_lock_irqsave(NULL);
+      flags = spin_lock_irqsave(&g_nrf52_gpio_lock);
 
       /* First, configure the port as a generic input so that we have a
        * known starting point and consistent behavior during the re-
@@ -308,7 +314,7 @@ int nrf52_gpio_config(nrf52_pinset_t cfgset)
           ret = -EINVAL;
         }
 
-      spin_unlock_irqrestore(NULL, flags);
+      spin_unlock_irqrestore(&g_nrf52_gpio_lock, flags);
     }
   else
     {
--- arch/arm/src/nrf91/nrf91_gpio.c
+++ arch/arm/src/nrf91/nrf91_gpio.c
@@ -37,6 +37,12 @@
 #include "nrf91_gpio.h"
 
 /****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+static spinlock_t g_nrf91_gpio_lock = SP_UNLOCKED;
+
+/****************************************************************************
  * Private Functions
  ****************************************************************************/
 
@@ -269,7 +275,7 @@ int nrf91_gpio_config(nrf91_pinset_t cfgset)
 
       pin = GPIO_PIN_DECODE(cfgset);
 
-      flags = spin_lock_irqsave(NULL);
+      flags = spin_lock_irqsave(&g_nrf91_gpio_lock);
 
       /* First, configure the port as a generic input so that we have a
        * known starting point and consistent behavior during the re-
@@ -304,7 +310,7 @@ int nrf91_gpio_config(nrf91_pinset_t cfgset)
           ret = -EINVAL;
         }
 
-      spin_unlock_irqrestore(NULL, flags);
+      spin_unlock_irqrestore(&g_nrf91_gpio_lock, flags);
     }
   else
     {
