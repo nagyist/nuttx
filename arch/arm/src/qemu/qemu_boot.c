cd1ed48ecac78be8fd0f22d1668b487035921ac9
Yanfeng Liu <p-liuyanfeng9@xiaomi.com>
2025-01-17 15:12:02 +0800

    arm/chip/qemu: add chip QEMU_CORTEXR5
    This adds support for QEMU Cortex-R5 virtual processor on existing QEMU
    Cortex-A7 code base with profile support in `armv7-r/` and `armv7-a/`.
    
    Signed-off-by: Yanfeng Liu <p-liuyanfeng9@xiaomi.com>

--- arch/arm/src/qemu/qemu_boot.c
+++ arch/arm/src/qemu/qemu_boot.c
@@ -25,7 +25,10 @@
 #include <nuttx/config.h>
 
 #include "arm_internal.h"
-#include "arm_cpu_psci.h"
+
+#ifdef CONFIG_ARM_PSCI
+#  include "arm_cpu_psci.h"
+#endif
 
 #include "qemu_irq.h"
 #include "qemu_memorymap.h"
@@ -39,6 +42,10 @@
 #ifdef CONFIG_SCHED_INSTRUMENTATION
 #  include <sched/sched.h>
 #  include <nuttx/sched_note.h>
+#endif
+
+#ifdef CONFIG_ARCH_ARMV7R
+#  include <nuttx/init.h>
 #endif
 
 #include <nuttx/syslog/syslog_rpmsg.h>
@@ -71,9 +78,11 @@ void arm_boot(void)
   up_perf_init(0);
 #endif
 
+#ifdef CONFIG_ARCH_ARMV7A
   /* Set the page table for section */
 
   qemu_setupmappings();
+#endif
 
   arm_fpuconfig();
 
@@ -95,6 +104,12 @@ void arm_boot(void)
 
 #ifdef CONFIG_SYSLOG_RPMSG
   syslog_rpmsg_init_early(g_syslog_rpmsg_buf, sizeof(g_syslog_rpmsg_buf));
+#endif
+
+#ifdef CONFIG_ARCH_ARMV7R
+  /* dont return per armv7-r/arm_head.S design */
+
+  nx_start();
 #endif
 }
 
