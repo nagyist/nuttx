9d2623a6e7411bfdf2b293698ea78ce9ac0a6d47
anjiahao <anjiahao@xiaomi.com>
2025-02-13 10:47:28 +0800

    remove task start hook [2/2]
    Signed-off-by: anjiahao <anjiahao@xiaomi.com>

--- sched/task/task_start.c
+++ sched/task/task_start.c
@@ -69,9 +69,6 @@ void nxtask_start(void)
 {
   FAR struct tcb_s *tcb = this_task();
   uint8_t ttype = tcb->flags & TCB_FLAG_TTYPE_MASK;
-#ifdef CONFIG_SCHED_STARTHOOK
-  FAR struct task_tcb_s *ttcb = (FAR struct task_tcb_s *)tcb;
-#endif
   int exitcode = EXIT_FAILURE;
   FAR char **argv;
   int argc;
