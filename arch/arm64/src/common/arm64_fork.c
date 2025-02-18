630db603a5beffaed2b26d02da294e21af99ffa6
wangmingrong1 <wangmingrong1@xiaomi.com>
2025-02-18 11:18:43 +0800

    arm64: Initialize sctrl_el1 when running fork
    Signed-off-by: wangmingrong1 <wangmingrong1@xiaomi.com>

--- arch/arm64/src/common/arm64_fork.c
+++ arch/arm64/src/common/arm64_fork.c
@@ -221,6 +221,11 @@ pid_t arm64_fork(const struct fork_s *context)
   child->xcp.regs[REG_SPSR]  |= (DAIF_IRQ_BIT | DAIF_FIQ_BIT);
 #endif /* CONFIG_SUPPRESS_INTERRUPTS */
 
+  child->xcp.regs[REG_SCTLR_EL1] = read_sysreg(sctlr_el1);
+#ifdef CONFIG_ARM64_MTE
+  child->xcp.regs[REG_SCTLR_EL1] |= SCTLR_TCF1_BIT;
+#endif
+
   child->xcp.regs[REG_ELR]    = (uint64_t)context->lr;
 
   child->xcp.regs[REG_EXE_DEPTH] = 0;
