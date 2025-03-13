ff910d039f126d21215ed9ae1afedb459d6c10e4
wangmingrong1 <wangmingrong1@xiaomi.com>
2025-03-13 18:43:52 +0800

    gcov.py: Generate code coverage data separately, reducing memory requirements
    Use multithreading to speed up conversion when there are multiple copies of data
    
    Signed-off-by: wangmingrong1 <wangmingrong1@xiaomi.com>

--- tools/gcov.py
+++ tools/gcov.py
@@ -24,6 +24,8 @@ import re
 import shutil
 import subprocess
 import sys
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
 
 
 def parse_gcda_data(path):
@@ -113,12 +115,70 @@ def copy_file_endswith(endswith, source, target):
                 shutil.copy2(src_file, dst_file)
 
 
+def run_lcov(data_dir, gcov_tool):
+    output = data_dir + ".info"
+    # lcov collect coverage data to coverage.info
+    command = [
+        "lcov",
+        "-c",
+        "-o",
+        output,
+        "--rc",
+        "lcov_branch_coverage=1",
+        "--gcov-tool",
+        gcov_tool,
+        "--ignore-errors",
+        "gcov",
+        "--directory",
+        data_dir,
+    ]
+    print(command)
+    subprocess.run(
+        command,
+        check=True,
+        stdout=sys.stdout,
+        stderr=sys.stdout,
+    )
+
+    return output
+
+
+def run_genhtml(infos: list[str], report):
+    cmd = [
+        "genhtml",
+        "--branch-coverage",
+        "-o",
+        report,
+        "--ignore-errors",
+        "source",
+    ]
+    cmd += infos
+    subprocess.run(
+        cmd,
+        check=True,
+        stdout=sys.stdout,
+        stderr=sys.stdout,
+    )
+
+
+def multithread_lcov(gcda_dir, gcno_dir, gcov_tool, delete):
+    copy_file_endswith(".gcno", gcno_dir, gcda_dir)
+    path = run_lcov(gcda_dir, gcov_tool)
+    if delete:
+        for file in Path(gcda_dir).rglob("*.gcno"):
+            file.unlink()
+
+    return path
+
+
 def arg_parser():
     parser = argparse.ArgumentParser(
         description="Code coverage generation tool.", add_help=False
     )
     parser.add_argument("-t", dest="gcov_tool", help="Path to gcov tool")
     parser.add_argument("-b", dest="base_dir", help="Compile base directory")
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode")
+    parser.add_argument("--delete", action="store_true", help="Delete gcda files")
     parser.add_argument(
         "-s",
         dest="gcno_dir",
@@ -132,7 +192,6 @@ def arg_parser():
         nargs="+",
         help="Directory containing gcda files",
     )
-    parser.add_argument("--debug", action="store_true", help="Enable debug mode")
     parser.add_argument(
         "-x",
         dest="only_copy",
@@ -157,7 +216,6 @@ def main():
     result_dir = os.path.abspath(args.result_dir)
 
     os.makedirs(result_dir, exist_ok=True)
-    data_dir = os.path.join(result_dir, "data")
     report_dir = os.path.join(result_dir, "report")
     coverage_file = os.path.join(result_dir, "coverage.info")
 
@@ -169,21 +227,10 @@ def main():
     for i in args.gcda_dir:
         if os.path.isfile(i):
             gcda_dirs.append(parse_gcda_data(os.path.join(root_dir, i)))
+            if args.delete:
+                os.remove(i)
         else:
             gcda_dirs.append(os.path.abspath(i))
-
-    # Copy all data together with the path to the data_dir directory
-    shield = []
-    for i in gcda_dirs:
-        abs_path = os.path.abspath(i)
-        target_dir = os.path.join(data_dir, os.path.basename(abs_path))
-        shield.append(target_dir)
-        copy_file_endswith(".gcda", abs_path, target_dir)
-        copy_file_endswith(".gcno", gcno_dir, target_dir)
-
-    # Only copy files
-    if args.only_copy:
-        sys.exit(0)
 
     # lcov tool is required
     if shutil.which("lcov") is None:
@@ -192,50 +239,27 @@ def main():
         )
         sys.exit(1)
 
-    try:
-
-        # lcov collect coverage data to coverage_file
-        command = [
-            "lcov",
-            "-c",
-            "-o",
-            coverage_file,
-            "--rc",
-            "lcov_branch_coverage=1",
-            "--gcov-tool",
-            args.gcov_tool,
-            "--ignore-errors",
-            "gcov",
-            "--directory",
-            f"{data_dir}",
+    cpu = min(os.cpu_count() or 1, len(gcda_dirs))
+    coverage_files = []
+    with ThreadPoolExecutor(max_workers=cpu) as executor:
+        futures = [
+            executor.submit(multithread_lcov, i, gcno_dir, args.gcov_tool, args.delete)
+            for i in gcda_dirs
         ]
+        coverage_files = [future.result() for future in futures]
 
-        print(command)
+    # Only copy files
+    if args.only_copy:
+        sys.exit(0)
 
-        subprocess.run(
-            command,
-            check=True,
-            stdout=sys.stdout,
-            stderr=sys.stdout,
-        )
+    try:
+        with ThreadPoolExecutor(max_workers=cpu) as executor:
+            for coverage_file in coverage_files:
+                executor.submit(
+                    correct_content_path, coverage_file, gcda_dirs, args.base_dir
+                )
 
-        correct_content_path(coverage_file, shield, args.base_dir)
-
-        # genhtml generate coverage report
-        subprocess.run(
-            [
-                "genhtml",
-                "--branch-coverage",
-                "-o",
-                report_dir,
-                coverage_file,
-                "--ignore-errors",
-                "source",
-            ],
-            check=True,
-            stdout=sys.stdout,
-            stderr=sys.stdout,
-        )
+        run_genhtml(coverage_files, report_dir)
 
         print(
             "Copy the following link and open it in the browser to view the coverage report:"
