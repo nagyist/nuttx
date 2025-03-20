1c228a72f67a99c56376a850eee66f23fed8b591
wanggang26 <wanggang26@xiaomi.com>
2025-03-20 09:11:07 +0800

    mutex: consolidate these two macro definitions to facilitate code maintenance
    Signed-off-by: wanggang26 <wanggang26@xiaomi.com>

--- include/nuttx/mutex.h
+++ include/nuttx/mutex.h
@@ -37,6 +37,7 @@
  ****************************************************************************/
 
 #define NXMUTEX_NO_HOLDER      ((pid_t)-1)
+#define NXMUTEX_RESET          ((pid_t)-2)
 #define NXMUTEX_INITIALIZER    {NXSEM_INITIALIZER(1, SEM_TYPE_MUTEX | \
                                 SEM_PRIO_INHERIT), NXMUTEX_NO_HOLDER}
 #define NXRMUTEX_INITIALIZER   {NXMUTEX_INITIALIZER, 0}
