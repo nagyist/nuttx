d46782327f645c0365b93703b100a8a2b32bc626
buxiasen <buxiasen@xiaomi.com>
2025-04-16 16:27:46 +0800

    arch/risc-v/cmake: fix typo of toolchain
    After fix GCCVER is correct
    
    Signed-off-by: buxiasen <buxiasen@xiaomi.com>

--- arch/risc-v/src/cmake/Toolchain.cmake
+++ arch/risc-v/src/cmake/Toolchain.cmake
@@ -259,7 +259,8 @@ if(CONFIG_RISCV_TOOLCHAIN STREQUAL GNU_RVG)
   if(NOT GCCVER)
     execute_process(COMMAND ${CMAKE_CXX_COMPILER} --version
                     OUTPUT_VARIABLE GCC_VERSION_OUTPUT)
-    string(REGEX MATCH "[0-9]+\\.[0-9]+" GCC_VERSION "${GCC_VERSION_INFO}")
+    string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)" _
+                 "${GCC_VERSION_OUTPUT}")
     set(GCCVER ${CMAKE_MATCH_1})
 
     if(GCCVER GREATER_EQUAL 12)
