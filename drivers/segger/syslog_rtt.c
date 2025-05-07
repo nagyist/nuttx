83e87ba458a2093317c2d8600747b7e6116723f6
liwenxiang1 <liwenxiang1@xiaomi.com>
2025-05-07 11:05:34 +0800

    drivers/segger: Remove macros related to FIFO mode settings
    Signed-off-by: liwenxiang1 <liwenxiang1@xiaomi.com>

--- drivers/segger/syslog_rtt.c
+++ drivers/segger/syslog_rtt.c
@@ -34,7 +34,6 @@
 
 int syslog_rtt_putc(FAR syslog_channel_t *channel, int ch)
 {
-  SEGGER_RTT_BLOCK_IF_FIFO_FULL(CONFIG_SYSLOG_RTT_CHANNEL);
   SEGGER_RTT_PutChar(CONFIG_SYSLOG_RTT_CHANNEL, ch);
   return ch;
 }
@@ -42,6 +41,5 @@ int syslog_rtt_putc(FAR syslog_channel_t *channel, int ch)
 ssize_t syslog_rtt_write(FAR syslog_channel_t *channel,
                          FAR const char *buffer, size_t buflen)
 {
-  SEGGER_RTT_BLOCK_IF_FIFO_FULL(CONFIG_SYSLOG_RTT_CHANNEL);
   return SEGGER_RTT_Write(CONFIG_SYSLOG_RTT_CHANNEL, buffer, buflen);
 }
