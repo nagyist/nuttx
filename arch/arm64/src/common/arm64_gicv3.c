86f1b255d4437b46421dfd2c0240b6a11115bde9
wangmingrong1 <wangmingrong1@xiaomi.com>
2025-05-08 20:47:43 +0800

    arm64: Used by irrelevant macro switch position
    Add unused_code to avoid compilation errors
    
    Signed-off-by: wangmingrong1 <wangmingrong1@xiaomi.com>

--- arch/arm64/src/common/arm64_gicv3.c
+++ arch/arm64/src/common/arm64_gicv3.c
@@ -27,6 +27,7 @@
 #include <assert.h>
 
 #include <nuttx/arch.h>
+#include <nuttx/compiler.h>
 #include <arch/barriers.h>
 #include <arch/irq.h>
 #include <arch/chip/chip.h>
@@ -86,7 +87,8 @@ static unsigned long g_gic_rdists[CONFIG_SMP_NCPUS];
  * Private Functions
  ***************************************************************************/
 
-static inline void sys_set_bit(unsigned long addr, unsigned int bit)
+unused_code static inline void sys_set_bit(unsigned long addr,
+                                           unsigned int bit)
 {
   uint32_t temp;
 
@@ -104,7 +106,8 @@ static inline void sys_clear_bit(unsigned long addr, unsigned int bit)
   putreg32(temp, addr);
 }
 
-static inline int sys_test_bit(unsigned long addr, unsigned int bit)
+unused_code static inline int sys_test_bit(unsigned long addr,
+                                           unsigned int bit)
 {
   uint32_t temp;
 
