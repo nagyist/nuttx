313e9f93ba83fed90eb1a7abedff5c0b5a016395
jingfei <jingfei@xiaomi.com>
2025-05-12 20:32:45 +0800

    adjust ftl for RRAM
    Accessing RRAM from the FTL interface can
    cause errors because RRAM lacks an erase interface.
    To make RRAM compatible with FTL, the FTL layer code
    needs to be modified.
    
    Signed-off-by: jingfei <jingfei@xiaomi.com>

--- drivers/mtd/ftl.c
+++ drivers/mtd/ftl.c
@@ -406,13 +406,14 @@ static ssize_t ftl_mtd_erase(FAR struct ftl_struct_s *dev, off_t startblock)
   if (dev->lptable == NULL)
     {
       ret = MTD_ERASE(dev->mtd, startblock, 1);
-      if (ret < 0)
+      if (ret != 1 && ret != -ENOSYS)
         {
           ferr("ERROR: Erase block %" PRIdOFF " failed: %zd\n",
                startblock, ret);
+          return ret;
         }
 
-      return ret;
+      return 1;
     }
 
   while (1)
@@ -423,9 +424,9 @@ static ssize_t ftl_mtd_erase(FAR struct ftl_struct_s *dev, off_t startblock)
         }
 
       ret = MTD_ERASE(dev->mtd, dev->lptable[startblock], 1);
-      if (ret == 1)
+      if (ret == 1 || ret == -ENOSYS)
         {
-          return ret;
+          return 1;
         }
 
       MTD_MARKBAD(dev->mtd, dev->lptable[startblock]);
