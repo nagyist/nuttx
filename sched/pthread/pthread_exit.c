b6ed2991fcd0374d01eef202bda29bfd1ed66fdd
hujun5 <hujun5@xiaomi.com>
2025-05-26 18:33:02 +0800

    pthread: The modification of tcb->flags should be carried out within the critical section.
    Signed-off-by: hujun5 <hujun5@xiaomi.com>

--- sched/pthread/pthread_exit.c
+++ sched/pthread/pthread_exit.c
@@ -65,6 +65,7 @@
 void nx_pthread_exit(FAR void *exit_value)
 {
   FAR struct tcb_s *tcb = this_task();
+  irqstate_t flags;
   sigset_t set;
   int status;
 
@@ -72,7 +73,9 @@ void nx_pthread_exit(FAR void *exit_value)
 
   DEBUGASSERT(tcb != NULL);
 
+  flags = enter_critical_section();
   tcb->flags |= TCB_FLAG_EXIT_PROCESSING;
+  leave_critical_section(flags);
 
   /* Block any signal actions that would awaken us while were
    * are performing the JOIN handshake.
