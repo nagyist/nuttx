6c7988d7ec2de8313734a231c196eccf43266b35
Xiang Xiao <xiaoxiang@xiaomi.com>
2025-06-30 13:40:06 +0800

    libc/machine/xtensa: Rename mcount.S to arch_mcount.S
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- libs/libc/machine/xtensa/arch_mcount.c
+++ libs/libc/machine/xtensa/arch_mcount.c
@@ -1,5 +1,5 @@
 /****************************************************************************
- * libs/libc/machine/xtensa/mcount.c
+ * libs/libc/machine/xtensa/arch_mcount.c
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
