0ed4aa67a120b7284c374fd085db35af48d3fac3
Bowen Wang <wangbowen6@xiaomi.com>
2025-07-09 20:39:58 +0800

    mtd/rpmsgmtd: wait bound with rpmsg mtd server in all operations
    all the rpmsg mtd operations should wait until bound with rpmsg
    mtd server.
    
    Signed-off-by: Bowen Wang <wangbowen6@xiaomi.com>

--- drivers/mtd/rpmsgmtd.c
+++ drivers/mtd/rpmsgmtd.c
@@ -748,13 +748,22 @@ static int rpmsgmtd_send_recv(FAR struct rpmsgmtd_s *priv,
 
   if (copy)
     {
-      ret = rpmsg_send(&priv->ept, msg, len);
-    }
-  else
-    {
-      ret = rpmsg_send_nocopy(&priv->ept, msg, len);
+      uint32_t buflen;
+      FAR void *buf;
+
+      buf = rpmsgmtd_get_tx_payload_buffer(priv, &buflen);
+      if (buf == NULL)
+        {
+          ret = -ENOMEM;
+          goto fail;
+        }
+
+      DEBUGASSERT(len <= buflen);
+      memcpy(buf, msg, len);
+      msg = buf;
     }
 
+  ret = rpmsg_send_nocopy(&priv->ept, msg, len);
   if (ret < 0)
     {
       goto fail;
