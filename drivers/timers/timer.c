76df96bc16d54121ce3800874c3e209e6ab7bb2d
fangpeina <fangpeina@xiaomi.com>
2025-07-23 21:38:41 +0800

    drivers/timers: Remove redundant check for crefs > 0 before decrement
    The reference count (upper->crefs) is incremented in timer_open()
    only after ensuring it will not overflow, timer_close() is only
    called after a successful open. Therefore, crefs should never be
    zero when timer_close() is called. The check for crefs > 0 before
    decrementing is unnecessary and has been removed to simplify the code.
    
    Signed-off-by: fangpeina <fangpeina@xiaomi.com>

--- drivers/timers/timer.c
+++ drivers/timers/timer.c
@@ -196,16 +196,7 @@ static int timer_close(FAR struct file *filep)
   /* Get exclusive access to the device structures */
 
   nxmutex_lock(&upper->lock);
-
-  /* Decrement the references to the driver.  If the reference count will
-   * decrement to 0, then uninitialize the driver.
-   */
-
-  if (upper->crefs > 0)
-    {
-      upper->crefs--;
-    }
-
+  upper->crefs--;
   nxmutex_unlock(&upper->lock);
   return OK;
 }
