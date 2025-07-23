2636e00db85316989a90879799f0506b764ba2af
fangpeina <fangpeina@xiaomi.com>
2025-07-23 21:35:58 +0800

    drivers/timers: remove return value check for nxmutex_lock
    The nxmutex_lock() function is a block wait operation
    and is not expected to fail under normal circumstances.
    
    Signed-off-by: fangpeina <fangpeina@xiaomi.com>

--- drivers/timers/timer.c
+++ drivers/timers/timer.c
@@ -152,11 +152,7 @@ static int timer_open(FAR struct file *filep)
 
   /* Get exclusive access to the device structures */
 
-  ret = nxmutex_lock(&upper->lock);
-  if (ret < 0)
-    {
-      goto errout;
-    }
+  nxmutex_lock(&upper->lock);
 
   /* Increment the count of references to the device.  If this the first
    * time that the driver has been opened for this device, then initialize
@@ -179,8 +175,6 @@ static int timer_open(FAR struct file *filep)
 
 errout_with_lock:
   nxmutex_unlock(&upper->lock);
-
-errout:
   return ret;
 }
 
@@ -196,17 +190,12 @@ static int timer_close(FAR struct file *filep)
 {
   FAR struct inode *inode = filep->f_inode;
   FAR struct timer_upperhalf_s *upper = inode->i_private;
-  int ret;
 
   tmrinfo("crefs: %d\n", upper->crefs);
 
   /* Get exclusive access to the device structures */
 
-  ret = nxmutex_lock(&upper->lock);
-  if (ret < 0)
-    {
-      return ret;
-    }
+  nxmutex_lock(&upper->lock);
 
   /* Decrement the references to the driver.  If the reference count will
    * decrement to 0, then uninitialize the driver.
@@ -275,11 +264,7 @@ static int timer_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
 
   /* Get exclusive access to the device structures */
 
-  ret = nxmutex_lock(&upper->lock);
-  if (ret < 0)
-    {
-      return ret;
-    }
+  nxmutex_lock(&upper->lock);
 
   /* Handle built-in ioctl commands */
 
