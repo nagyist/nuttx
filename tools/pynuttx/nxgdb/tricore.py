c2876aea8a18284c5187bacf70e83c58b0273d46
yangao1 <yangao1@xiaomi.com>
2025-08-27 16:53:04 +0800

    tricore.py:for lower/upper csa,the location of PC is different
    Signed-off-by: yangao1 <yangao1@xiaomi.com>

--- tools/pynuttx/nxgdb/tricore.py
+++ tools/pynuttx/nxgdb/tricore.py
@@ -28,7 +28,47 @@ from .backtrace import Backtrace
 
 FCX_FREE_MASK = (0xFFFF << 0) | (0xF << 16)
 PCXI_UL = 1 << 20
+REG_LPC = 1
 REG_UPC = 3
+
+# Register offset in CSA
+
+lower_offsets = [
+    ("LPCXI", 0),
+    ("LA11", 1),  # LPC
+    ("A2", 2),
+    ("A3", 3),
+    ("D0", 4),
+    ("D1", 5),
+    ("D2", 6),
+    ("D3", 7),
+    ("A4", 8),
+    ("A5", 9),
+    ("A6", 10),
+    ("A7", 11),
+    ("D4", 12),
+    ("D5", 13),
+    ("D6", 14),
+    ("D7", 15),
+]
+upper_offsets = [
+    ("UPCXI", 0),
+    ("PSW", 1),
+    ("A10", 2),
+    ("UA11", 3),  # UPC
+    ("D8", 4),
+    ("D9", 5),
+    ("D10", 6),
+    ("D11", 7),
+    ("A12", 8),
+    ("A13", 9),
+    ("A14", 10),
+    ("A15", 11),
+    ("D12", 12),
+    ("D13", 13),
+    ("D14", 14),
+    ("D15", 15),
+]
 
 
 class TricoreCSA(gdb.Command):
@@ -38,26 +78,29 @@ class TricoreCSA(gdb.Command):
         return (csa & 0x000F0000) << 12 | (csa & 0x0000FFFF) << 6
 
     def get_pcxi_from_tcb(self, tcb):
-        if utils.task_is_running(tcb):
-            frame = gdb.newest_frame()
-            pcxi = frame.read_register("pcx") & FCX_FREE_MASK
-            return self.csa2addr(pcxi)
-        else:
-            return int(tcb["xcp"]["regs"])
+        return int(tcb["xcp"]["regs"])
 
-    def dump_csa(self, pid, pcxi):
+    def dump_csa(self, pid, csa_addr):
         print(f"pid:{pid}")
         address = []
-        while pcxi != 0:
-            addr = utils.read_uint(pcxi)
-            pc = utils.read_uint(pcxi + REG_UPC * 4)
-            is_upper = bool(addr & PCXI_UL)
+        is_upper = False
+        line = ""
+        while csa_addr != 0:
+            offsets = upper_offsets if is_upper else lower_offsets
+            pc = utils.read_uint(csa_addr + (REG_UPC if is_upper else REG_LPC) * 4)
             if pc:
-                print(f"pcxi:{hex(pcxi)} upper:{is_upper} pc:0x{pc:X}")
                 address.append(pc)
+            print(f"CSA addr:{hex(csa_addr)} is {'upper' if is_upper else 'lower'}")
+            for i, (name, offset) in enumerate(offsets, 1):
+                val = utils.read_uint(csa_addr + offset * 4)
+                line += f"{name}:0x{val:08X}  "
+                if i % 4 == 0:
+                    print(line)
+                    line = ""
 
-            pcxi = FCX_FREE_MASK & addr
-            pcxi = self.csa2addr(pcxi)
+            pcxi = utils.read_uint(csa_addr)  # next CSA
+            is_upper = bool(pcxi & PCXI_UL)
+            csa_addr = self.csa2addr(pcxi & FCX_FREE_MASK)
         print(str(Backtrace(address)))
 
     def handle_all(self):
@@ -74,8 +117,8 @@ class TricoreCSA(gdb.Command):
             return
         self.dump_csa(pid, self.get_pcxi_from_tcb(tcb))
 
-    def handle_pcxi(self, pcxi):
-        self.dump_csa(-1, pcxi)
+    def handle_pcxi(self, csa_addr):
+        self.dump_csa(-1, csa_addr)
 
     def get_argparser(self):
         parser = argparse.ArgumentParser(description=self.__doc__)
