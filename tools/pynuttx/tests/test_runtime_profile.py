734c2d1a3a0b6b5b163edf33504d109b0d71a74f
wangmingrong1 <wangmingrong1@xiaomi.com>
2025-09-08 20:56:34 +0800

    nxgdb/test: test profile adapt to higher version gdb profile modification
    The higher version of gdb does not have the printing.py file
    
    Signed-off-by: wangmingrong1 <wangmingrong1@xiaomi.com>

--- tools/pynuttx/tests/test_runtime_profile.py
+++ tools/pynuttx/tests/test_runtime_profile.py
@@ -39,7 +39,7 @@ class TestProfile(unittest.TestCase):
 
         out = gdb.execute(" profile p 0x123", to_string=True)
         self.check_output(out, expect="function calls in")
-        self.check_output(out, expect="printing.py")
+        self.check_output(out, expect="_gdb.execute")
         # Example of output:
 
     # (gdb) profile p 0x123
@@ -49,13 +49,9 @@ class TestProfile(unittest.TestCase):
     #    Ordered by: cumulative time
 
     #    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
-    #         1    0.000    0.000    0.001    0.001 {built-in method builtins.exec}
+    #         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
     #         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
     #         1    0.000    0.000    0.000    0.000 {built-in method _gdb.execute}
-    #         2    0.000    0.000    0.000    0.000 printing.py:205(__call__)
-    #         2    0.000    0.000    0.000    0.000 types.py:22(get_basic_type)
-    #         2    0.000    0.000    0.000    0.000 {method 'search' of 're.Pattern' objects}
-    #         2    0.000    0.000    0.000    0.000 {method 'unqualified' of 'gdb.Type' objects}
     #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 
     def test_time(self):
