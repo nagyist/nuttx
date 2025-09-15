d1493a4a52a66e56f3f88abff64a103ddc7d79a6
p-szafonimateusz <p-szafonimateusz@xiaomi.com>
2025-09-15 12:02:17 +0800

    libc/pthread/pthread_keydelete.c: reset key value
    When key is deleted, its value should also be reset.
    
    This fixes the pthread_getspecific.c test case from
    PSE52 Open Group Threads Test Suite.
    
    Signed-off-by: p-szafonimateusz <p-szafonimateusz@xiaomi.com>

--- libs/libc/pthread/pthread_keydelete.c
+++ libs/libc/pthread/pthread_keydelete.c
@@ -60,6 +60,7 @@
 int pthread_key_delete(pthread_key_t key)
 {
   FAR struct task_info_s *info = task_get_info();
+  FAR struct tls_info_s *tls = tls_get_info();
   int ret = EINVAL;
 
   DEBUGASSERT(info != NULL);
@@ -74,6 +75,7 @@ int pthread_key_delete(pthread_key_t key)
       if (ret == OK)
         {
           info->ta_tlsdtor[key] = NULL;
+          tls->tl_elem[key]     = 0;
           nxmutex_unlock(&info->ta_lock);
         }
       else
