5318f9cc517ec884d259e9042689f4c1ac9f8254
niyinghao <niyinghao@xiaomi.com>
2025-09-25 20:27:26 +0800

    v4l2-m2m: support userptr mode
    Signed-off-by: niyinghao <niyinghao@xiaomi.com>

--- drivers/video/v4l2_m2m.c
+++ drivers/video/v4l2_m2m.c
@@ -350,6 +350,7 @@ static int codec_querybuf(FAR struct file *filep,
   FAR codec_mng_t *cmng = inode->i_private;
   FAR codec_file_t *cfile = filep->f_priv;
   FAR codec_type_inf_t *type_inf;
+  FAR vbuf_container_t *container;
   struct v4l2_format format;
   uint32_t offset = 0;
   size_t bufsize;
@@ -388,6 +389,8 @@ static int codec_querybuf(FAR struct file *filep,
       return -EINVAL;
     }
 
+  container = video_framebuff_find_container(&type_inf->bufinf, buf->index);
+
   if (V4L2_TYPE_IS_MULTIPLANAR(buf->type))
     {
       for (i = 0; i < format.fmt.pix_mp.num_planes; i++)
@@ -402,6 +405,11 @@ static int codec_querybuf(FAR struct file *filep,
                 offset += buf->m.planes[i].length;
                 break;
 
+              case V4L2_MEMORY_USERPTR:
+                buf->m.planes[i].m.userptr = container ?
+                    container->buf.m.planes[i].m.userptr : 0;
+                break;
+
               default:
                 return -EINVAL;
             }
@@ -414,6 +422,10 @@ static int codec_querybuf(FAR struct file *filep,
           case V4L2_MEMORY_MMAP:
             buf->length   = bufsize;
             buf->m.offset = offset + bufsize * buf->index;
+            break;
+
+          case V4L2_MEMORY_USERPTR:
+            buf->m.userptr = container ? container->buf.m.userptr : 0;
             break;
 
           default:
@@ -481,6 +493,11 @@ static int codec_qbuf(FAR struct file *filep,
                      offset + type_inf->bufheap);
                 break;
 
+              case V4L2_MEMORY_USERPTR:
+                container->buf.m.planes[i].m.vaddr =
+                    (FAR void *)buf->m.planes[i].m.userptr;
+                break;
+
               default:
                 return -EINVAL;
             }
@@ -493,6 +510,10 @@ static int codec_qbuf(FAR struct file *filep,
           case V4L2_MEMORY_MMAP:
             container->buf.m.vaddr =
                 (FAR void *)(type_inf->bufheap + buf->m.offset - offset);
+            break;
+
+          case V4L2_MEMORY_USERPTR:
+            container->buf.m.vaddr = (FAR void *)buf->m.userptr;
             break;
 
           default:
@@ -573,6 +594,11 @@ static int codec_dqbuf(FAR struct file *filep,
                      type_inf->bufheap + offset);
                 break;
 
+              case V4L2_MEMORY_USERPTR:
+                buf->m.planes[i].m.userptr = (unsigned long)
+                    buf->m.planes[i].m.vaddr;
+                break;
+
               default:
                 return -EINVAL;
             }
@@ -585,6 +611,10 @@ static int codec_dqbuf(FAR struct file *filep,
           case V4L2_MEMORY_MMAP:
             buf->m.offset = (uint32_t)((uint8_t *)buf->m.vaddr -
                             type_inf->bufheap + offset);
+            break;
+
+          case V4L2_MEMORY_USERPTR:
+            buf->m.userptr = (unsigned long)buf->m.vaddr;
             break;
 
           default:
