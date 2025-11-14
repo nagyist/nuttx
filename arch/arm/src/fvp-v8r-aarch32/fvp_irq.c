3ca91ecc722486774f7925e60c316187d647f2a9
v-tangmeng <v-tangmeng@xiaomi.com>
2025-11-14 14:21:25 +0800

    arch/arm: fix the compile error
    ld: nuttx/staging/libsched.a(irq_initialize.o): in function `irq_initialize':
    nuttx/sched/irq/irq_initialize.c:96: undefined reference to `up_irqinitialize'
    
    Signed-off-by: v-tangmeng <v-tangmeng@xiaomi.com>

---
+++ arch/arm/src/fvp-v8r-aarch32/fvp_irq.c
@@ -0,0 +1,70 @@
+/****************************************************************************
+ * arch/arm/src/fvp-v8r-aarch32/fvp_irq.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <nuttx/irq.h>
+#include <nuttx/arch.h>
+#include <arch/irq.h>
+
+#include "gic.h"
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: up_irqinitialize
+ *
+ * Description:
+ *   This function is called by up_initialize() during the bring-up of the
+ *   system.  It is the responsibility of this function to but the interrupt
+ *   subsystem into the working and ready state.
+ *
+ ****************************************************************************/
+
+void up_irqinitialize(void)
+{
+  /* The following operations need to be atomic, but since this function is
+   * called early in the initialization sequence, we expect to have exclusive
+   * access to the GIC.
+   */
+
+  /* Initialize the Generic Interrupt Controller (GIC) for CPU0 */
+
+  if (this_cpu() == 0)
+    {
+      arm_gic0_initialize();      /* for GIC common parts */
+    }
+
+  arm_gic_initialize();       /* for GIC CPU interface */
+
+#ifndef CONFIG_SUPPRESS_INTERRUPTS
+
+  /* And finally, enable interrupts */
+
+  arm_color_intstack();
+  up_irq_enable();
+#endif
+}
