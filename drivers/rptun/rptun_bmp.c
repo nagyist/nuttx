10b08d2104fdd59e8a0e44ee6f03d76145480869
yintao <yintao@xiaomi.com>
2025-11-21 10:21:43 +0800

    rptun/bmp: Set synchronization point for master
    If the resource table is initialized by slave,
    the master may get uninitialized rsc
    
    Signed-off-by: yintao <yintao@xiaomi.com>

--- drivers/rptun/rptun_bmp.c
+++ drivers/rptun/rptun_bmp.c
@@ -97,12 +97,9 @@ rptun_bmp_get_resource(FAR struct rptun_dev_s *dev)
 {
   FAR struct rptun_bmp_dev_s *priv = (FAR struct rptun_bmp_dev_s *)dev;
 
-  if (!priv->master)
+  while (priv->rsc->rsc_tbl_hdr.ver == 0)
     {
-      while (priv->rsc->rsc_tbl_hdr.ver == 0)
-        {
-          nxsig_usleep(100);
-        }
+      nxsig_usleep(100);
     }
 
   return &priv->rsc->rsc_tbl_hdr;
