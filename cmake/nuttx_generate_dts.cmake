b33e1417882386e2546026fee0be1c04e36643e2
fangpeina <fangpeina@xiaomi.com>
2025-12-03 16:33:01 +0800

    Revert "cmake: support devicetree header file generation in build [multiple board]"
    This reverts commit 5bf1cde685b25791ac89c3379c03982e1b275a64.
    
    Reason for revert: remove DTS_DYNAMIC_API_SUPPORT
    
    Signed-off-by: fangpeina <fangpeina@xiaomi.com>

--- cmake/nuttx_generate_dts.cmake
+++ cmake/nuttx_generate_dts.cmake
@@ -27,6 +27,9 @@ set(CMAKE_BINARY_DIR ${CMAKE_ARGV5})
 include(cmake/nuttx_kconfig.cmake)
 nuttx_export_kconfig(${KCONFIG_CONFIG})
 
+set(DTS_SRCS "")
+set(CUSTOM_OVERLAY_DTS_FILE "")
+
 # The directory containing devicetree related scripts.
 set(DT_SCRIPTS_DIR ${NUTTX_DIR}/tools/devicetree)
 
@@ -35,7 +38,6 @@ set(GEN_EDT_SCRIPT ${DT_SCRIPTS_DIR}/gen_edt.py)
 # This generates DT information needed by the C macro APIs, along with a few
 # other things.
 set(GEN_DEFINES_SCRIPT ${DT_SCRIPTS_DIR}/gen_defines.py)
-set(GEN_MULTIPLE_DEFINED_SCRIPT ${DT_SCRIPTS_DIR}/gen_dynamic_api.py)
 # The generated file containing the final DTS, for debugging.
 set(DTS_FILE ${CMAKE_BINARY_DIR}/dt/nuttx.dts)
 # The edtlib.EDT object in pickle format.
@@ -46,59 +48,22 @@ set(GEN_DEFINES ${CMAKE_BINARY_DIR}/include/nuttx/devicetree_generated.h)
 set(DTS_PRE_FILE ${CMAKE_BINARY_DIR}/dt/nuttx.dts.pre)
 set(DTS_DEPS_FILE ${CMAKE_BINARY_DIR}/dt/nuttx.dts.pre.d)
 
+# The source files to be preprocessed.
+set(DT_PREPROCESS_SOURCE_FILES "")
+# The root directory of the devicetree bindings files.
+set(DTS_ROOT_BINDINGS ${NUTTX_DIR}/devicetree/bindings)
+# The include directories for the devicetree preprocessor.
+set(DTS_PREPROCESS_INCLUDE_DIRECTORIES
+    ${NUTTX_DIR}/include ${NUTTX_DIR}/devicetree ${NUTTX_DIR}/devicetree/common)
+
 # Create a directory for the devicetree output files.
 if(NOT EXISTS ${CMAKE_BINARY_DIR}/dt)
   file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/dt)
 endif()
 
-set(BOARD_NUM)
-set(CURRENT_BOARD_ID 0)
-
-function(parse_dts_config idx)
-  math(EXPR idx "${idx} - 1")
-  # Get current board devicetree configs argumennts
-  list(LENGTH CONFIG_DTS_SOURCE_DTR list_length)
-  if(idx LESS list_length)
-    list(GET CONFIG_DTS_SOURCE_DTR ${idx} dts_source_dir)
-  else()
-    set(dts_source_dir "")
-  endif()
-
-  list(LENGTH CONFIG_DTS_OVERLAY_DIR list_length)
-  if(idx LESS list_length)
-    list(GET CONFIG_DTS_OVERLAY_DIR ${idx} dts_overlay_dir)
-  else()
-    set(dts_overlay_dir "")
-  endif()
-
-  list(LENGTH CONFIG_DTS_CUSTOM_BINDINGS_DIR list_length)
-  if(idx LESS list_length)
-    list(GET CONFIG_DTS_CUSTOM_BINDINGS_DIR ${idx} dts_custom_bindings_dir)
-  else()
-    set(dts_custom_bindings_dir "")
-  endif()
-
-  list(LENGTH CONFIG_DTS_CUSTOM_DT_BINDINGS_INCLUDE_DIR list_length)
-  if(idx LESS list_length)
-    list(GET CONFIG_DTS_CUSTOM_DT_BINDINGS_INCLUDE_DIR ${idx}
-         dts_custom_dt_bindings_include_dir)
-  else()
-    set(dts_custom_dt_bindings_include_dir "")
-  endif()
-
-  set(DTS_SRCS "")
-  set(CUSTOM_OVERLAY_DTS_FILE "")
-  # The source files to be preprocessed.
-  set(DT_PREPROCESS_SOURCE_FILES "")
-  # The root directory of the devicetree bindings files.
-  set(DTS_ROOT_BINDINGS ${NUTTX_DIR}/devicetree/bindings)
-  # The include directories for the devicetree preprocessor.
-  set(DTS_PREPROCESS_INCLUDE_DIRECTORIES
-      ${NUTTX_DIR}/include ${NUTTX_DIR}/devicetree
-      ${NUTTX_DIR}/devicetree/common)
-
+function(parse_dts_config)
   # Get .dts files from defconfig
-  string(REGEX REPLACE "," ";" dirlist "${dts_source_dir}")
+  string(REGEX REPLACE "," ";" dirlist "${CONFIG_DTS_SOURCE_DTR}")
   foreach(dir ${dirlist})
     get_filename_component(abs_dir ${dir} ABSOLUTE)
     if(IS_DIRECTORY ${abs_dir})
@@ -108,7 +73,7 @@ function(parse_dts_config idx)
   endforeach()
 
   # Get .overlay files from defconfig
-  string(REGEX REPLACE "," ";" dirlist "${dts_overlay_dir}")
+  string(REGEX REPLACE "," ";" dirlist "${CONFIG_DTS_OVERLAY_DIR}")
   foreach(dir ${dirlist})
     get_filename_component(abs_dir ${dir} ABSOLUTE)
     if(IS_DIRECTORY ${abs_dir})
@@ -128,7 +93,7 @@ function(parse_dts_config idx)
   list(APPEND DT_PREPROCESS_SOURCE_FILES ${DTS_SRCS} ${CUSTOM_OVERLAY_DTS_FILE})
 
   # Get custom bindings directory from defconfig
-  string(REGEX REPLACE "," ";" dirlist "${dts_custom_bindings_dir}")
+  string(REGEX REPLACE "," ";" dirlist "${CONFIG_DTS_CUSTOM_BINDINGS_DIR}")
   set(abs_dirlist "")
   foreach(dir ${dirlist})
     get_filename_component(abs_dir ${dir} ABSOLUTE)
@@ -138,7 +103,8 @@ function(parse_dts_config idx)
 
   # Get custom dt-bindings directory from defconfig
   # CONFIG_DTS_CUSTOM_DT_BINDINGS_INCLUDE_DIR
-  string(REGEX REPLACE "," ";" dirlist "${dts_custom_dt_bindings_include_dir}")
+  string(REGEX REPLACE "," ";" dirlist
+                       "${CONFIG_DTS_CUSTOM_DT_BINDINGS_INCLUDE_DIR}")
   set(abs_dirlist "")
   foreach(dir ${dirlist})
     get_filename_component(abs_dir ${dir} ABSOLUTE)
@@ -195,20 +161,6 @@ function(dts_preprocess)
                 (error code ${ret}): ${DT_PREPROCESS_SOURCE_FILES}")
   endif()
   message(STATUS "Generated nuttx.dts.pre: ${DTS_PRE_FILE}")
-  message(STATUS "CMD_PREPROCESS: ${CMD_PREPROCESS}")
-
-  message(STATUS "CURRENT_BOARD_ID: ${CURRENT_BOARD_ID}")
-  message(STATUS "BOARD_NUM: ${BOARD_NUM}")
-  if("${CONFIG_DTS_DYNAMIC_API_SUPPORT}" STREQUAL "y")
-    string(REGEX REPLACE "nuttx.dts.pre" "nuttx_${CURRENT_BOARD_ID}.dts.pre"
-                         new_dts_pre_file "${DTS_PRE_FILE}")
-    file(COPY_FILE ${DTS_PRE_FILE} ${new_dts_pre_file} ONLY_IF_DIFFERENT)
-    string(REGEX
-           REPLACE "nuttx.dts.pre.d" "nuttx_${CURRENT_BOARD_ID}.dts.pre.d"
-                   new_dts_deps_file "${DTS_DEPS_FILE}")
-    file(COPY_FILE ${DTS_DEPS_FILE} ${new_dts_deps_file} ONLY_IF_DIFFERENT)
-  endif()
-
 endfunction()
 
 # Generate edt pickle file using gen_edt.py
@@ -239,87 +191,19 @@ function(gen_edt)
   file(REMOVE ${DTS_FILE}.new ${EDT_PICKLE_FILE}.new)
   message(STATUS "Generated nuttx.dts: ${DTS_FILE}")
   message(STATUS "Generated pickled edt: ${EDT_PICKLE_FILE}")
-  message(STATUS "CMD_GEN_EDT: ${CMD_GEN_EDT}")
-
-  if("${CONFIG_DTS_DYNAMIC_API_SUPPORT}" STREQUAL "y")
-    string(REGEX REPLACE "nuttx.dts" "nuttx_${CURRENT_BOARD_ID}.dts"
-                         new_dts_file "${DTS_FILE}")
-    file(COPY_FILE ${DTS_FILE} ${new_dts_file} ONLY_IF_DIFFERENT)
-    string(REGEX REPLACE "edt.pickle" "edt_${CURRENT_BOARD_ID}.pickle"
-                         new_edt_pickle_file "${EDT_PICKLE_FILE}")
-    file(COPY_FILE ${EDT_PICKLE_FILE} ${new_edt_pickle_file} ONLY_IF_DIFFERENT)
-  endif()
 endfunction(gen_edt)
 
 # Generate devicetree_generated.h using gen_defines.py
 function(gen_devicetree_header)
-  set(CMD_GEN_DEFINES
-      ${PYTHON_EXECUTABLE}
-      ${GEN_DEFINES_SCRIPT}
-      --edt-pickle
-      ${EDT_PICKLE_FILE}
-      --header-out
-      ${GEN_DEFINES}.new)
-  if(BOARD_NUM EQUAL 1)
-    execute_process(
-      COMMAND ${CMD_GEN_DEFINES}
-      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-      RESULT_VARIABLE GEN_DEFINES_RESULT)
-    file(COPY_FILE ${GEN_DEFINES}.new ${GEN_DEFINES} ONLY_IF_DIFFERENT)
-    file(REMOVE ${GEN_DEFINES}.new)
-    if(NOT "${CONFIG_DTS_DYNAMIC_API_SUPPORT}" STREQUAL "y")
-      return()
-    endif()
-  endif()
-
-  if(CURRENT_BOARD_ID EQUAL 1)
-    execute_process(
-      COMMAND ${CMD_GEN_DEFINES}
-      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-      RESULT_VARIABLE GEN_DEFINES_RESULT)
-    string(REGEX REPLACE "devicetree_generated.h" "devicetree_generated_base.h"
-                         gen_defines_base "${GEN_DEFINES}")
-    file(COPY_FILE ${GEN_DEFINES}.new ${gen_defines_base} ONLY_IF_DIFFERENT)
-    file(REMOVE ${GEN_DEFINES}.new)
-    message(STATUS "Generated devicetree_generated_base.h: ${gen_defines_base}")
-  endif()
-
-  if("${CONFIG_DTS_DYNAMIC_API_SUPPORT}" STREQUAL "y")
-    string(
-      REGEX
-      REPLACE "devicetree_generated.h"
-              "devicetree_generated_${CURRENT_BOARD_ID}.h" current_gen_defines
-              "${GEN_DEFINES}")
-    set(CMD_GEN_DEFINES
-        ${PYTHON_EXECUTABLE}
-        ${GEN_DEFINES_SCRIPT}
-        --edt-pickle
-        ${EDT_PICKLE_FILE}
-        --header-out
-        ${GEN_DEFINES}.new
-        --board-id
-        "B${CURRENT_BOARD_ID}")
-  endif()
-
+  set(CMD_GEN_DEFINES ${PYTHON_EXECUTABLE} ${GEN_DEFINES_SCRIPT} --edt-pickle
+                      ${EDT_PICKLE_FILE} --header-out ${GEN_DEFINES}.new)
   execute_process(
     COMMAND ${CMD_GEN_DEFINES}
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     RESULT_VARIABLE GEN_DEFINES_RESULT)
-  message(STATUS "CMD_GEN_DEFINES: ${CMD_GEN_DEFINES}")
-  file(COPY_FILE ${GEN_DEFINES}.new ${current_gen_defines} ONLY_IF_DIFFERENT)
+  file(COPY_FILE ${GEN_DEFINES}.new ${GEN_DEFINES} ONLY_IF_DIFFERENT)
   file(REMOVE ${GEN_DEFINES}.new)
   message(STATUS "Generated devicetree_generated.h: ${GEN_DEFINES}")
-endfunction()
-
-function(gen_multi_devicetree_header)
-  set(CMD_GEN_MULTIPLE_DEFINES
-      ${PYTHON_EXECUTABLE} ${GEN_MULTIPLE_DEFINED_SCRIPT} --header-out
-      "${GEN_DEFINES}" --board-nums ${BOARD_NUM})
-  message(STATUS "CMD_GEN_MULTIPLE_DEFINES: ${CMD_GEN_MULTIPLE_DEFINES}")
-  execute_process(
-    COMMAND ${CMD_GEN_MULTIPLE_DEFINES}
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-    RESULT_VARIABLE GEN_DEFINES_RESULT)
 endfunction()
 
 # ~~~
@@ -336,31 +220,21 @@ function(nuttx_generate_dts)
     return()
   endif()
 
-  list(LENGTH CONFIG_DTS_SOURCE_DTR dts_count)
-  set(BOARD_NUM ${dts_count})
+  parse_dts_config()
 
-  foreach(i RANGE 1 ${BOARD_NUM})
-    message(STATUS "Current board id: ${i}")
-    parse_dts_config(${i})
-
-    # Check if DTS_SRCS is empty
-    if("${DTS_SRCS}" STREQUAL "")
-      message(STATUS "no device tree source files specified (DTS_SRCS),
-                    skipped device tree generation")
-    else()
-      math(EXPR CURRENT_BOARD_ID "${CURRENT_BOARD_ID} + 1")
-      # Preprocess device tree source files
-      dts_preprocess()
-      # Generate edt pickle file using gen_edt.py
-      gen_edt()
-      # Generate devicetree_generated.h using gen_defines.py
-      gen_devicetree_header()
-    endif()
-  endforeach()
-
-  if("${CONFIG_DTS_DYNAMIC_API_SUPPORT}" STREQUAL "y")
-    gen_multi_devicetree_header()
+  # Check if DTS_SRCS is empty
+  if("${DTS_SRCS}" STREQUAL "")
+    message(STATUS "no device tree source files specified (DTS_SRCS),
+                skipped device tree generation")
+    return()
   endif()
+
+  # Preprocess device tree source files
+  dts_preprocess()
+  # Generate edt pickle file using gen_edt.py
+  gen_edt()
+  # Generate devicetree_generated.h using gen_defines.py
+  gen_devicetree_header()
 
 endfunction()
 
