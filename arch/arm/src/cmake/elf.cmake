ccef76a8877e36be4b99b8631de6b009e0ef1be3
haopengxiang <haopengxiang@xiaomi.com>
2026-03-13 14:11:18 +0800

    cmake/arm: remove duplicate LTO config block in elf.cmake
    The if(CONFIG_BINFMT_ELF_RELOCATABLE) block and
    nuttx_mod_compile_options_ifdef for CONFIG_LTO_FULL at lines 54-57
    were exact duplicates of the block at lines 49-52. Remove the
    redundant copy.
    
    Signed-off-by: haopengxiang <haopengxiang@xiaomi.com>

--- arch/arm/src/cmake/elf.cmake
+++ arch/arm/src/cmake/elf.cmake
@@ -51,9 +51,4 @@ if(CONFIG_BINFMT_ELF_RELOCATABLE)
 endif()
 nuttx_mod_compile_options_ifdef(CONFIG_LTO_FULL -fno-lto)
 
-if(CONFIG_BINFMT_ELF_RELOCATABLE)
-  nuttx_elf_compile_options_ifdef(CONFIG_LTO_FULL -fno-lto)
-endif()
-nuttx_mod_compile_options_ifdef(CONFIG_LTO_FULL -fno-lto)
-
 nuttx_elf_link_options(-e __start)
