Donny9/vela-nuttx

timerfd: notify application of system relatime changes
refs : https://man7.org/linux/man-pages/man2/timerfd_create.2.html

If the associated clock is either CLOCK_REALTIME or
CLOCK_REALTIME_ALARM, the timer is absolute
(TFD_TIMER_ABSTIME), and the flag TFD_TIMER_CANCEL_ON_SET
was specified when calling timerfd_settime(), then read(2)
fails with the error ECANCELED if the real-time clock
undergoes a discontinuous change.  (This allows the
reading application to discover such discontinuous changes
to the clock.)

examples code:
{
    int nevents = epoll_wait(epollfd, events, N_ANDROID_TIMERFDS, -1);
    if (nevents < 0) {
        return nevents;
    }

    int result = 0;
    for (int i = 0; i < nevents; i++) {
        uint32_t alarm_idx = events[i].data.u32;
        uint64_t unused;
        ssize_t err = read(fds[alarm_idx], &unused, sizeof(unused));
        if (err < 0 && errno != EAGAIN) {
            if (errno == ECANCELED) {
                //get time changed
            } else {
                return err;
            }
        } else {
           //....
        }
    }
}

drivers/timers/rpmsg_rtc.c
@@ -27,6 +27,7 @@
 #include <nuttx/nuttx.h>
 #include <nuttx/list.h>
 #include <nuttx/clock.h>
+#include <nuttx/clock_changed_notifier.h>
 #include <nuttx/kmalloc.h>
 #include <nuttx/rpmsg/rpmsg.h>
 #include <nuttx/mutex.h>
@@ -306,8 +307,13 @@ static int rpmsg_rtc_ept_cb(FAR struct rpmsg_endpoint *ept, FAR void *data,
           struct rpmsg_rtc_set_s *msg = data;
 
 #ifdef CONFIG_RTC_RPMSG_SYNC_BASETIME
+          struct timespec ts;
+
           g_basetime.tv_sec  = msg->base_sec;
           g_basetime.tv_nsec = msg->base_nsec;
+
+          clock_gettime(CLOCK_REALTIME, &ts);
+          clock_changed_notifier_call_chain(CLOCK_REALTIME, &ts);
 #else
           struct timespec tp;
 

sched/clock/clock_initialize.c
@@ -38,6 +38,7 @@
 
 #include <nuttx/arch.h>
 #include <nuttx/clock.h>
+#include <nuttx/clock_changed_notifier.h>
 #include <nuttx/trace.h>
 
 #include "clock/clock.h"
@@ -184,6 +185,8 @@ static void clock_inittime(FAR const struct timespec *tp)
       g_basetime.tv_nsec += NSEC_PER_SEC;
       g_basetime.tv_sec--;
     }
+
+  clock_changed_notifier_call_chain(CLOCK_REALTIME, &g_basetime);
 #else
   clock_inittimekeeping(tp);
 #endif

sched/clock/clock_settime.c
@@ -32,6 +32,7 @@
 #include <debug.h>
 
 #include <nuttx/arch.h>
+#include <nuttx/clock_changed_notifier.h>
 #include <nuttx/irq.h>
 #include <sys/time.h>
 
@@ -79,6 +80,7 @@ void nxclock_settime(clockid_t clock_id, FAR const struct timespec *tp)
 
   clock_systime_timespec(&bias);
   clock_timespec_subtract(tp, &bias, &g_basetime);
+  clock_changed_notifier_call_chain(CLOCK_REALTIME, tp);
 
   leave_critical_section(flags);
 

sched/clock/clock_timekeeping.c
@@ -36,6 +36,7 @@
 
 #include <nuttx/irq.h>
 #include <nuttx/arch.h>
+#include <nuttx/clock_changed_notifier.h>
 
 #include "clock/clock.h"
 
@@ -132,6 +133,7 @@ int clock_timekeeping_set_wall_time(FAR const struct timespec *ts)
     }
 
   memcpy(&g_clock_wall_time, ts, sizeof(struct timespec));
+  clock_changed_notifier_call_chain(CLOCK_REALTIME, ts);
 
   g_clock_adjust       = 0;
   g_clock_last_counter = counter;
@@ -291,6 +293,7 @@ void clock_inittimekeeping(FAR const struct timespec *tp)
       clock_basetime(&g_clock_wall_time);
     }
 
+  clock_changed_notifier_call_chain(CLOCK_REALTIME, &g_clock_wall_time);
   up_timer_gettick(&g_clock_last_counter);
 }
 

nagyist/nuttx

Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

drivers/timers/rpmsg_rtc.c
@@ -29,6 +29,7 @@
 #include <nuttx/nuttx.h>
 #include <nuttx/list.h>
 #include <nuttx/clock.h>
+#include <nuttx/clock_notifier.h>
 #include <nuttx/kmalloc.h>
 #include <nuttx/rpmsg/rpmsg.h>
 #include <nuttx/mutex.h>
@@ -302,12 +303,16 @@ static int rpmsg_rtc_ept_cb(FAR struct rpmsg_endpoint *ept, FAR void *data,
           struct rpmsg_rtc_set_s *msg = data;
 
 #ifdef CONFIG_RTC_RPMSG_SYNC_BASETIME
+          struct timespec ts;
           irqstate_t flags;
 
           flags = spin_lock_irqsave(&g_basetime_lock);
           g_basetime.tv_sec  = msg->base_sec;
           g_basetime.tv_nsec = msg->base_nsec;
           spin_unlock_irqrestore(&g_basetime_lock, flags);
+
+          clock_gettime(CLOCK_REALTIME, &ts);
+          clock_notifier_call_chain(CLOCK_REALTIME, &ts);
 #else
           struct timespec tp;
 

sched/clock/clock_initialize.c
@@ -38,6 +38,7 @@
 
 #include <nuttx/arch.h>
 #include <nuttx/clock.h>
+#include <nuttx/clock_notifier.h>
 #include <nuttx/trace.h>
 
 #include <nuttx/spinlock.h>
@@ -191,6 +192,7 @@ static void clock_inittime(FAR const struct timespec *tp)
     }
 
   spin_unlock_irqrestore(&g_basetime_lock, flags);
+  clock_notifier_call_chain(CLOCK_REALTIME, &g_basetime);
 #else
   clock_inittimekeeping(tp);
 #endif

sched/clock/clock_settime.c
@@ -34,6 +34,7 @@
 
 #include <nuttx/arch.h>
 #include <nuttx/fs/fs.h>
+#include <nuttx/clock_notifier.h>
 #include <nuttx/irq.h>
 #include <nuttx/spinlock.h>
 #include <nuttx/timers/ptp_clock.h>
@@ -72,6 +73,7 @@ static void nxclock_set_realtime(FAR const struct timespec *tp)
   flags = spin_lock_irqsave(&g_basetime_lock);
 
   clock_timespec_subtract(tp, &bias, &g_basetime);
+  clock_notifier_call_chain(CLOCK_REALTIME, tp);
 
   spin_unlock_irqrestore(&g_basetime_lock, flags);
 

sched/clock/clock_timekeeping.c
@@ -36,6 +36,7 @@
 
 #include <nuttx/irq.h>
 #include <nuttx/arch.h>
+#include <nuttx/clock_notifier.h>
 
 #include "clock/clock.h"
 
@@ -133,6 +134,7 @@ int clock_timekeeping_set_wall_time(FAR const struct timespec *ts)
     }
 
   memcpy(&g_clock_wall_time, ts, sizeof(struct timespec));
+  clock_notifier_call_chain(CLOCK_REALTIME, ts);
 
   g_clock_adjust       = 0;
   g_clock_last_counter = counter;
@@ -295,6 +297,7 @@ void clock_inittimekeeping(FAR const struct timespec *tp)
       clock_basetime(&g_clock_wall_time);
     }
 
+  clock_notifier_call_chain(CLOCK_REALTIME, &g_clock_wall_time);
   up_timer_gettick(&g_clock_last_counter);
   spin_unlock_irqrestore(&g_clock_lock, flags);
 }