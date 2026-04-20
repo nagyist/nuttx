acassis/incubator-nuttx

Add support for the loading of ET_DYN objects
* build-globals.sh
  - Build the modlib_globals.S file used to resolve symbols when dynamically loading

* include/elf.h
  - Add definitions that may be found in shared objects

* include/nuttx/lib/modlib.h
  - Add fields to mod_loadifno_s:
    - Program headers
    - Exported symbols
    - Data section address
    - Padding requirement
    - Section index for dynamic symbol table
    - Number of symbols exported
  - Add prottotype for modlib_freesymtab

* libs/libc/dlfcn/lib_dlclose.c
  - Free the symbol table when the dll is closed

* libs/libc/dlfcn/lib_dlopen.c
  - Add dump of program headers to debug routine
  - Differentiate between ET_REL and ET_DYN objects

* libs/libc/machine/arm/armv7-m/arch_elf.c
  - Add handling of R_ARM_RELATIVE and R_ARM_JUMP slot relocation types

* libs/libc/modlib/Make.defs
  - Include new file containing library APIs used when loading shared objects

* libs/libc/modlib/modlib.h
  - Add parameter to modlib_readsym prototype
  - Add prototypes for:
    - modlib_insertsymtab
    - modlib_findglobal

* libs/libc/modlib/modlib_loadshdrs.c
  - Rename modlib_loadshdrs.c to modlib_loadhdrs.c
  - Rename modlib_loadshdrs to modlib_loadhdrs
  - Add code to load program headers

* libs/libc/modlib/modlib_bind.c
  - Call modlib_readsym with pointer to symbol table
  - Add modlib_relocatedyn to manage relocation of symbols with shared object (ET_DYN)
  - Differentiate between ET_REL and ET_DYN objects

* libs/libc/modlib/modlib_load.c
  - Calculate sizes and text/data addresses based on program headers rather than section headers

* libs/libc/modlib/modlib_symbols.c
  - Define entry point structure
  - Add offset parameter to modlib_symname() and use to find symbol names
  - Add symtab section header parameter to modlib_readsym()
  - Add offset parameter to modlib_symvalue() to locate symbol names
  - Add modlib_insertsyntab() to create a symbol table for exporting and resolution
  - Add findEP() to 

libs/libc/modlib/modlib.h
@@ -83,7 +83,7 @@ int modlib_findsymtab(FAR struct mod_loadinfo_s *loadinfo);
  ****************************************************************************/
 
 int modlib_readsym(FAR struct mod_loadinfo_s *loadinfo, int index,
-                   FAR Elf_Sym *sym);
+                   FAR Elf32_Sym *sym, FAR Elf32_Shdr *shdr);
 
 /****************************************************************************
  * Name: modlib_symvalue
@@ -110,13 +110,64 @@ int modlib_readsym(FAR struct mod_loadinfo_s *loadinfo, int index,
  ****************************************************************************/
 
 int modlib_symvalue(FAR struct module_s *modp,
-                    FAR struct mod_loadinfo_s *loadinfo, FAR Elf_Sym *sym);
+                    FAR struct mod_loadinfo_s *loadinfo, FAR Elf32_Sym *sym,
+ 		    Elf32_Off offset);
+
+ /****************************************************************************
+  * Name: modlib_insertsymtab
+  *
+  * Description:
+  *   Insert a symbol table for the current module.
+  *
+  * Input Parameters:
+  *   modp     - Module state information
+  *   loadinfo - Module load information
+  *   shdr     - Symbol table section header
+  *   sym      - Symbol table entry
+  *
+  * Returned Value:
+  *   0 (OK) is returned on success and a negated errno is returned on
+  *   failure.
+  *
+  *   ENOMEM - Symbol undefined and not provided via a symbol table
+  *
+  ****************************************************************************/
+ 
+int modlib_insertsymtab(FAR struct module_s *modp, 
+ 		        FAR struct mod_loadinfo_s *loadinfo, 
+                        FAR Elf32_Shdr *shdr,
+                        FAR Elf32_Sym *sym);
 
 /****************************************************************************
- * Name: modlib_loadshdrs
+ * Name: modlib_findglobal
  *
  * Description:
- *   Loads section headers into memory.
+ *   Find a symbol in the global symbol table
+ *
+ * Input Parameters:
+ *   modp     - Module state information
+ *   loadinfo - Module load information
+ *   shdr     - Symbol table section header
+ *   sym      - Symbol table entry
+ *
+ * Returned Value:
+ *   0 (OK) is returned on success and a negated errno is returned on
+ *   failure.
+ *
+ *   ENOMEM - Symbol undefined and not provided via a symbol table
+ *
+ ****************************************************************************/
+
+void *modlib_findglobal(FAR struct module_s *modp, 
+		        FAR struct mod_loadinfo_s *loadinfo, 
+                        FAR Elf32_Shdr *shdr,
+                        FAR Elf32_Sym *sym);
+
+/****************************************************************************
+ * Name: modlib_loadhdrs
+ *
+ * Description:
+ *   Loads program and section headers into memory.
  *
  * Returned Value:
  *   0 (OK) is returned on success and a negated errno is returned on
@@ -124,7 +175,7 @@ int modlib_symvalue(FAR struct module_s *modp,
  *
  ****************************************************************************/
 
-int modlib_loadshdrs(FAR struct mod_loadinfo_s *loadinfo);
+int modlib_loadhdrs(FAR struct mod_loadinfo_s *loadinfo);
 
 /****************************************************************************
  * Name: modlib_findsection

nagyist/nuttx

    * libs/libc/machine/arm/armv7-m/arch_elf.c
      - Fix cast of error message parameters
    * build-globals.sh
      - Build the modlib_globals.S file used to resolve symbols when dynamically loading
    * libs/libc/modlib/Make.defs
      - Build modlib_globals.S for all targets
    * libs/libc/modlib/modlib_load.c
      - Calculate sizes and text/data addresses based on program headers rather than section headers
      - Handle objects with no program headers
    * libs/libc/modlib/modlib_bind.c
      - Call modlib_readsym with pointer to symbol table
      - Add modlib_relocatedyn to manage relocation of symbols with shared object (ET_DYN)
      - Differentiate between ET_REL and ET_DYN objects
      - Use arch independent symbol ELF_R_SYM
      - Cast sizes to avoid warnings
    * include/elf.h
      - Add definitions that may be found in shared objects
    * include/nuttx/lib/modlib.h
      - Add parameter to modlib_readsym prototype
      - Add prototypes for:
        - modlib_insertsymtab
        - modlib_findglobal
      - Define Elf_Dyn and Elf_Off according to the elf architecture
      - Add fields to mod_loadifno_s:
        - Program headers
        - Exported symbols
        - Data section address
        - Padding requirement
        - Section index for dynamic symbol table
        - Number of symbols exported
      - Add prottotype for modlib_freesymtab
    * libs/libc/dlfcn/lib_dlclose.c
      - Free the symbol table when the dll is closed
    * libs/libc/dlfcn/lib_dlopen.c
      - Add dump of program headers to debug routine
      - Add handling of R_ARM_RELATIVE and R_ARM_JUMP slot relocation types
    * libs/libc/modlib/modlib_loadshdrs.c
      - Rename modlib_loadshdrs.c to modlib_loadhdrs.c
      - Rename modlib_loadshdrs to modlib_loadhdrs
      - Add code to load program headers
    * libs/libc/modlib/modlib_symbols.c
      - Define entry point structure
      - Add offset parameter to modlib_symname() and use to find symbol names
      - A

libs/libc/modlib/modlib.h
@@ -83,7 +83,7 @@ int modlib_findsymtab(FAR struct mod_loadinfo_s *loadinfo);
  ****************************************************************************/
 
 int modlib_readsym(FAR struct mod_loadinfo_s *loadinfo, int index,
-                   FAR Elf_Sym *sym);
+                   FAR Elf_Sym *sym, FAR Elf_Shdr *shdr);
 
 /****************************************************************************
  * Name: modlib_symvalue
@@ -110,13 +110,64 @@ int modlib_readsym(FAR struct mod_loadinfo_s *loadinfo, int index,
  ****************************************************************************/
 
 int modlib_symvalue(FAR struct module_s *modp,
-                    FAR struct mod_loadinfo_s *loadinfo, FAR Elf_Sym *sym);
+                    FAR struct mod_loadinfo_s *loadinfo, FAR Elf_Sym *sym,
+ 		    Elf_Off offset);
+
+ /****************************************************************************
+  * Name: modlib_insertsymtab
+  *
+  * Description:
+  *   Insert a symbol table for the current module.
+  *
+  * Input Parameters:
+  *   modp     - Module state information
+  *   loadinfo - Module load information
+  *   shdr     - Symbol table section header
+  *   sym      - Symbol table entry
+  *
+  * Returned Value:
+  *   0 (OK) is returned on success and a negated errno is returned on
+  *   failure.
+  *
+  *   ENOMEM - Symbol undefined and not provided via a symbol table
+  *
+  ****************************************************************************/
+ 
+int modlib_insertsymtab(FAR struct module_s *modp, 
+ 		        FAR struct mod_loadinfo_s *loadinfo, 
+                        FAR Elf_Shdr *shdr,
+                        FAR Elf_Sym *sym);
 
 /****************************************************************************
- * Name: modlib_loadshdrs
+ * Name: modlib_findglobal
  *
  * Description:
- *   Loads section headers into memory.
+ *   Find a symbol in the global symbol table
+ *
+ * Input Parameters:
+ *   modp     - Module state information
+ *   loadinfo - Module load information
+ *   shdr     - Symbol table section header
+ *   sym      - Symbol table entry
+ *
+ * Returned Value:
+ *   0 (OK) is returned on success and a negated errno is returned on
+ *   failure.
+ *
+ *   ENOMEM - Symbol undefined and not provided via a symbol table
+ *
+ ****************************************************************************/
+
+void *modlib_findglobal(FAR struct module_s *modp, 
+		        FAR struct mod_loadinfo_s *loadinfo, 
+                        FAR Elf_Shdr *shdr,
+                        FAR Elf_Sym *sym);
+
+/****************************************************************************
+ * Name: modlib_loadhdrs
+ *
+ * Description:
+ *   Loads program and section headers into memory.
  *
  * Returned Value:
  *   0 (OK) is returned on success and a negated errno is returned on
@@ -124,7 +175,7 @@ int modlib_symvalue(FAR struct module_s *modp,
  *
  ****************************************************************************/
 
-int modlib_loadshdrs(FAR struct mod_loadinfo_s *loadinfo);
+int modlib_loadhdrs(FAR struct mod_loadinfo_s *loadinfo);
 
 /****************************************************************************
  * Name: modlib_findsection